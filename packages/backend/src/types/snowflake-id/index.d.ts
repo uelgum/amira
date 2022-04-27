a6a53b06c9ed62d17a894ea8e4c843a6f5401d6d
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-04-28 00:58:50 +0200

    Types für snowflake-id hinzugefügt

---
+++ packages/backend/src/types/snowflake-id/index.d.ts
@@ -0,0 +1,11 @@
+declare module "snowflake-id" {
+    export type SnowflakeOptions = {
+        mid: number;
+        offset: number;
+    };
+
+    export default class Snowflake {
+        constructor(options?: SnowflakeOptions);
+        public generate(): string;
+    }
+}
