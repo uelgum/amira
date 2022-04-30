7e438d780fb5810c5b6cbab3e8714b1fef3e1493
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-04-30 19:38:38 +0200

    config hinzugefügt

---
+++ packages/backend/src/config.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import path from "path";
+import yml from "js-yaml";
+import configSchema from "@schemas/config";
+import root from "@utils/root";
+
+// #region Types
+type Config = {
+    port: number;
+    jwtKey: string;
+    mongo: {
+        host: string;
+        username: string;
+        password: string;
+        database: string;
+    };
+};
+// #endregion
+
+const CONFIG_PATH = path.join(root, "/config/config.yml");
+
+const file = fs.readFileSync(CONFIG_PATH, "utf-8");
+const config = yml.load(file) as Config;
+
+/**
+    Validiert die Config.
+*/
+const validateConfig = () => {
+    const { error } = configSchema.validate(config);
+
+    if(error) {
+        const message = error.details[0].message;
+        throw new Error(`Fehlerhafte Config: ${message}`);
+    }
+};
+
+export { validateConfig };
+export default config;
