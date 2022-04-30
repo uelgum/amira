d3c8f6d402380ef123e2b38a4dc91fa0bea2d34f
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-04-30 19:38:26 +0200

    AmiraError hinzugefügt

---
+++ packages/backend/src/structs/amiraError.ts
@@ -0,0 +1,28 @@
+/**
+    Error der Amira API.
+*/
+class AmiraError extends Error {
+    // #region Attribute
+    /**
+        HTTP-Status des Fehlers.
+    */
+    public readonly status: number;
+
+    /**
+        Fehlercode.
+    */
+    public readonly code: string;
+    // #endregion
+    
+    /**
+        Konstruktor von `AmiraError`.
+    */
+    constructor(status: number, code: string) {
+        super();
+
+        this.status = status;
+        this.code = code;
+    }
+}
+
+export default AmiraError;
