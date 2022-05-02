f0d1a293149d9eb9288e932e64b417f4bf4d0d74
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-02 22:13:04 +0200

    HTTP Middleware hinzugefügt

---
+++ packages/backend/src/middleware/http/isLoggedIn.ts
@@ -0,0 +1,39 @@
+import jwt from "jwt-promisify";
+import AmiraError from "@structs/amiraError";
+import { sendError } from "@utils/response";
+import config from "@config";
+import { Request, Response } from "express";
+
+// #region Types
+type Next = () => void;
+// #endregion
+
+/**
+    Überprüft, ob ein Nutzer angemeldet ist.
+*/
+const isLoggedIn = async (req: Request, res: Response, next: Next) => {
+    const header = req.headers.authorization;
+
+    if(!header) {
+        sendError(res, new AmiraError(401, "LOGIN_REQUIRED"));
+        return;
+    }
+
+    const [ schema, token ] = header.split(" ");
+
+    if(!token || schema !== "Bearer") {
+        sendError(res, new AmiraError(400, "INVALID_TOKEN"));
+        return;
+    }
+
+    try {
+        res.locals = await jwt.verify(token, config.jwtKey);
+    } catch(error) {
+        sendError(res, new AmiraError(400, "INVALID_TOKEN"));
+        return;
+    }
+
+    next();
+};
+
+export default isLoggedIn;
---
+++ packages/backend/src/middleware/http/isLoggedOut.ts
@@ -0,0 +1,23 @@
+import AmiraError from "@structs/amiraError";
+import { sendError } from "@utils/response";
+import { Request, Response } from "express";
+
+// #region Types
+type Next = () => void;
+// #endregion
+
+/**
+    Überprüft, ob ein Nutzer abgemeldet ist.
+*/
+const isLoggedOut = async (req: Request, res: Response, next: Next) => {
+    const header = req.headers.authorization;
+
+    if(header) {
+        sendError(res, new AmiraError(403, "LOGOUT_REQUIRED"));
+        return;
+    }
+
+    next();
+};
+
+export default isLoggedOut;
