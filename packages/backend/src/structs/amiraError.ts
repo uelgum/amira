f0beef91194c503da5e292fa46a72a6f756d9fc4
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-03 23:18:32 +0200

    subCode hinzugefügt

--- packages/backend/src/structs/amiraError.ts
+++ packages/backend/src/structs/amiraError.ts
@@ -12,16 +12,22 @@ class AmiraError extends Error {
         Fehlercode.
     */
     public readonly code: string;
+
+    /**
+        Optionaler untergeordneter Fehlercode.
+    */
+    public readonly subcode?: string;
     // #endregion
     
     /**
         Konstruktor von `AmiraError`.
     */
-    constructor(status: number, code: string) {
+    constructor(status: number, code: string, subcode?: string) {
         super();
 
         this.status = status;
         this.code = code;
+        this.subcode = subcode;
     }
 }
 
