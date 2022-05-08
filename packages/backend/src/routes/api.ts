36c25abf0ca2c8d7b5e18ab6ca3269ee8975141c
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-08 18:37:16 +0200

    Account-Routen hinzugefügt

--- packages/backend/src/routes/api.ts
+++ packages/backend/src/routes/api.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 // Routen
 import authRouter from "@routes/auth";
+import accountRouter from "@routes/account";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/account", accountRouter);
 
 export default router;
