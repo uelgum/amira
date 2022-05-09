71d2d66262099eddaefcbdd164e1886671d70a18
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-10 00:21:30 +0200

    Contact Service hinzugefügt

---
+++ packages/backend/src/services/contact.ts
@@ -0,0 +1,52 @@
+import Contact from "@models/contact";
+import User from "@models/user";
+import Notification from "@models/notification";
+import AmiraError from "@structs/amiraError";
+
+// Services
+import { generateId } from "@services/id";
+import { sendNotificationUpdateCommand } from "@services/notification";
+
+/**
+    Sendet eine Kontakt-Anfrage an einen Nutzer.
+*/
+const sendContactRequest = async (senderId: string, recipientId: string) => {
+    const contactExists = await Contact.exists({
+        $or: [
+            { contactId1: senderId, contactId2: recipientId },
+            { contactId1: recipientId, contactId2: senderId },
+        ]
+    });
+
+    if(contactExists) {
+        throw new AmiraError(409, "CONTACT_ERROR", "CONTACT_ALREADY_EXISTS");
+    }
+
+    const sender = await User.findOne({ id: senderId });
+
+    const contact = new Contact({
+        id: generateId(),
+        contactId1: senderId,
+        contactId2: recipientId,
+        confirmed: false
+    });
+    
+    const notification = new Notification({
+        id: generateId(),
+        type: "CONTACT_REQUEST",
+        recipientId,
+        data: {
+            name: `${sender.firstName} ${sender.lastName}`
+        },
+        createdAt: Date.now()
+    });
+
+    await contact.save();
+    await notification.save();
+
+    sendNotificationUpdateCommand(recipientId);
+};
+
+export {
+    sendContactRequest
+};
