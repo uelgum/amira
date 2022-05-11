62504af3e6b76a085b7f3109bf1d1179b525bb73
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-11 20:40:48 +0200

    isLoggedIn hinzugefügt

--- packages/backend/src/routes/account.ts
+++ packages/backend/src/routes/account.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from "express";
+import isLoggedIn from "@middleware/http/isLoggedIn";
 import { sendData, sendError } from "@utils/response";
 
 // Services
 import { verifyEmail } from "@services/account";
 
 const router = Router();
+
+router.use(isLoggedIn);
 
 // E-Mail-Verifizierung
 router.post("/verify-email", async (req: Request, res: Response) => {
