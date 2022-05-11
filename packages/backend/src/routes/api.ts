25cee01090abdee71144d31777a62aa2c4e93df3
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-11 22:17:17 +0200

    app-Router hinzugefügt

--- packages/backend/src/routes/api.ts
+++ packages/backend/src/routes/api.ts
@@ -4,11 +4,13 @@ import { Router } from "express";
 import authRouter from "@routes/auth";
 import accountRouter from "@routes/account";
 import contactRouter from "@routes/contact";
+import appRouter from "./apps";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/account", accountRouter);
 router.use("/contact", contactRouter);
+router.use("/apps", appRouter);
 
 export default router;
