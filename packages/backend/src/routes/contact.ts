baddf15ac1a31d43f672464b7604b7ed4c56a2cd
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-11 20:46:01 +0200

    Contact-Router hinzugefügt

---
+++ packages/backend/src/routes/contact.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from "express";
+import isLoggedIn from "@middleware/http/isLoggedIn";
+import { sendData, sendError } from "@utils/response";
+
+// Services
+import { acceptContactRequest, sendContactRequest } from "@services/contact";
+
+const router = Router();
+
+router.use(isLoggedIn);
+
+// Kontakt-Anfrage schicken
+router.post("/send-request", async (req: Request, res: Response) => {
+    try {
+        await sendContactRequest(res.locals.id, req.body.recipientId);
+        sendData(res, { success: true });
+    } catch(error) {
+        sendError(res, error);
+    }
+});
+
+// Kontakt-Anfrage annehmen
+router.post("/accept-request", async (req: Request, res: Response) => {
+    try {
+        await acceptContactRequest(res.locals.id, req.body.senderId);
+        sendData(res, { success: true });
+    } catch(error) {
+        sendError(res, error);
+    }
+});
+
+export default router;
