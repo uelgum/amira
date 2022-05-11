c443346086a90b3ffb86e61400f15cd7dadfc9d9
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-11 20:46:10 +0200

    Contact-Router hinzugefügt

--- packages/backend/src/routes/api.ts
+++ packages/backend/src/routes/api.ts
@@ -3,10 +3,12 @@ import { Router } from "express";
 // Routen
 import authRouter from "@routes/auth";
 import accountRouter from "@routes/account";
+import contactRouter from "@routes/contact";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/account", accountRouter);
+router.use("/contact", contactRouter);
 
 export default router;
