034e5155f847ba6341f3d8cf4f8ae0ff3ccf92ba
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-11 20:46:50 +0200

    acceptContactRequest hinzugefügt

--- packages/backend/src/services/contact.ts
+++ packages/backend/src/services/contact.ts
@@ -12,7 +12,7 @@ import { sendNotificationUpdateCommand } from "@services/notification";
 */
 const sendContactRequest = async (senderId: string, recipientId: string) => {
     if(!senderId || !recipientId || senderId === recipientId) {
-        throw new AmiraError(409, "CONTACT_ERROR", "INVALID_DATA");
+        throw new AmiraError(400, "CONTACT_ERROR", "INVALID_DATA");
     }
 
     const contactExists = await Contact.exists({
@@ -51,6 +51,46 @@ const sendContactRequest = async (senderId: string, recipientId: string) => {
     sendNotificationUpdateCommand(recipientId);
 };
 
+/**
+    Akzeptiert eine Kontakt-Anfrage.
+*/
+const acceptContactRequest = async (userId: string, senderId: string) => {
+    if(!userId || !senderId) {
+        throw new AmiraError(400, "CONTACT_ERROR", "INVALID_DATA");
+    }
+
+    const contact = await Contact.findOne({
+        $or: [
+            { contactId1: senderId, contactId2: userId },
+            { contactId1: userId, contactId2: senderId },
+        ]
+    });
+
+    if(!contact) {
+        throw new AmiraError(400, "CONTACT_ERROR", "CONTACT_NOT_FOUND");
+    }
+
+    contact.confirmed = true;
+
+    const recipient = await User.findOne({ id: userId });
+
+    const notification = new Notification({
+        id: generateId(),
+        type: "CONTACT_REQUEST_ACCEPTED",
+        recipientId: senderId,
+        data: {
+            name: `${recipient.firstName} ${recipient.lastName}`
+        },
+        createdAt: Date.now()
+    });
+
+    await contact.save();
+    await notification.save();
+
+    sendNotificationUpdateCommand(senderId);
+};
+
 export {
-    sendContactRequest
+    sendContactRequest,
+    acceptContactRequest
 };
