0091937dec199e1ada7a2e1eb495a08245b1d502
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-11 21:17:42 +0200

    getTasks hinzugefügt und fixes

--- packages/backend/src/services/apps/todoList.ts
+++ packages/backend/src/services/apps/todoList.ts
@@ -1,21 +1,25 @@
+import { Payload } from "jwt-promisify";
 import TodoList from "@models/todoList";
 import User from "@models/user";
 import AmiraError from "@structs/amiraError";
 
 // Services
 import { generateId } from "@services/id";
-import { encrypt } from "@services/crypto";
+import { encrypt, decrypt } from "@services/crypto";
 import { validateTodoListData } from "@services/validation";
 
 /**
     Fügt eine neue Aufgabe zur Todo-Liste hinzu.
 */
-const addTask = async (userId: string, data: Record<string, any>) => {
+const addTask = async (payload: Payload, data: Record<string, any>) => {
     const { error, subcode } = validateTodoListData(data);
 
     if(error) {
         throw new AmiraError(400, "APP_ERROR", subcode);
     }
+
+    const userId = payload.id;
+    const passwordKey = payload.key;
 
     const todoListExists = await TodoList.exists({ userId });
 
@@ -32,9 +36,12 @@ const addTask = async (userId: string, data: Record<string, any>) => {
     const user = await User.findOne({ id: userId });
     const todoList = await TodoList.findOne({ userId });
 
+    // User-Schlüssel entschlüsseln mit Passwort-Key aus Payload
+    const userKey = decrypt(user.key, passwordKey);
+
     const task = {
         id: generateId(),
-        content: encrypt(data.content, user.key),
+        content: encrypt(data.content, userKey),
         important: false,
         done: false,
         createdAt: Date.now()
@@ -44,6 +51,36 @@ const addTask = async (userId: string, data: Record<string, any>) => {
     await todoList.save();
 };
 
+/**
+    Ruft alle Aufgaben der Todo-Liste ab.
+*/
+const getTasks = async (payload: Payload) => {
+    const userId = payload.id;
+    const passwordKey = payload.key;
+
+    const user = await User.findOne({ id: userId });
+    const todoList = await TodoList.findOne({ userId });
+
+    if(!todoList) {
+        throw new AmiraError(400, "APP_ERROR", "INVALID_DATA");
+    }
+
+    // User-Schlüssel entschlüsseln mit Passwort-Key aus Payload
+    const userKey = decrypt(user.key, passwordKey);
+
+    const tasks = todoList.tasks.map((task) => {
+        return {
+            id: task.id,
+            content: decrypt(task.content, userKey),
+            important: task.important,
+            done: task.done
+        };
+    });
+
+    return tasks;
+};
+
 export {
-    addTask
+    addTask,
+    getTasks
 };
