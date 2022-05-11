9de2bb259a05dd1cd571941ac303149bc7ccc4e8
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-11 22:19:21 +0200

    Todo Router hinzugefügt

---
+++ packages/backend/src/routes/apps/todoList.ts
@@ -0,0 +1,54 @@
+import { Router, Request, Response } from "express";
+
+// Services
+import {
+    getTasks,
+    addTask,
+    updateTask,
+    deleteTask
+} from "@services/apps/todoList";
+import { sendData, sendError } from "@utils/response";
+
+const router = Router();
+
+// Aufgaben abrufen
+router.get("/get", async (req: Request, res: Response) => {
+    try {
+        const tasks = await getTasks(res.locals);
+        sendData(res, { tasks });
+    } catch(error) {
+        sendError(res, error);
+    }
+});
+
+// Aufgabe hinzufügen
+router.post("/add", async (req: Request, res: Response) => {
+    try {
+        await addTask(res.locals, req.body);
+        sendData(res, { sucess: true });
+    } catch(error) {
+        sendError(res, error);
+    }
+});
+
+// Aufgabe aktualisieren
+router.post("/update", async (req: Request, res: Response) => {
+    try {
+        await updateTask(res.locals, req.body);
+        sendData(res, { sucess: true });
+    } catch(error) {
+        sendError(res, error);
+    }
+});
+
+// Aufgabe entfernen
+router.post("/delete", async (req: Request, res: Response) => {
+    try {
+        await deleteTask(res.locals, req.body);
+        sendData(res, { sucess: true });
+    } catch(error) {
+        sendError(res, error);
+    }
+});
+
+export default router;
