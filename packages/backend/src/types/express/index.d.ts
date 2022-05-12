1160fa17fd2708edf39d99e140822a384b749dab
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-13 00:26:13 +0200

    admin und emailUnverified hinzugefügt

--- packages/backend/src/types/express/index.d.ts
+++ packages/backend/src/types/express/index.d.ts
@@ -2,26 +2,14 @@ import "express";
 
 declare module "express" {
     export type Locals = {
-        /**
-            ID des Nutzers.
-        */
         id: string;
-
-        /**
-            Vorname des Nutzers.
-        */
         name: string;
-
-        /**
-            Passwort-Schlüssel des Nutzers.
-        */
         key: string;
+        admin?: boolean;
+        emailUnverified?: boolean;
     };
 
     export interface Response {
-        /**
-            Informationen über den Nutzer.
-        */
         locals: Locals;
     }
 }
