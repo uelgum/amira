8f6d117c53717e422ca2089bf7464adcdc802d95
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-12 20:56:29 +0200

    handleJsonError hinzugefügt

---
+++ packages/backend/src/middleware/http/handleJsonError.ts
@@ -0,0 +1,21 @@
+import { Response } from "express";
+import { sendError } from "@utils/response";
+import AmiraError from "@structs/amiraError";
+
+// #region Types
+type Next = () => void;
+// #endregion
+
+/**
+    Verarbeitet einen JSON Fehler aufgrund fehlerhafter Formatierung.
+*/
+const handleJsonError = (error: Error, _, res: Response, next: Next) => {
+    if(error instanceof SyntaxError && error["body"]) {
+        sendError(res, new AmiraError(400, "BAD_REQUEST", "MALFORMED_JSON"));
+        return;
+    }
+
+    next();
+};
+
+export default handleJsonError;
