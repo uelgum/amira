ba4ceec8be1c8a15e9f58113172baf2a3310cc53
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-13 00:14:50 +0200

    Typ verbessert

--- packages/backend/src/types/express/index.d.ts
+++ packages/backend/src/types/express/index.d.ts
@@ -1,11 +1,27 @@
 import "express";
 
 declare module "express" {
+    export type Locals = {
+        /**
+            ID des Nutzers.
+        */
+        id: string;
+
+        /**
+            Vorname des Nutzers.
+        */
+        name: string;
+
+        /**
+            Passwort-Schlüssel des Nutzers.
+        */
+        key: string;
+    };
+
     export interface Response {
-        locals: {
-            id: string;
-            name: string;
-            key: string;
-        };
+        /**
+            Informationen über den Nutzer.
+        */
+        locals: Locals;
     }
 }
