fe02b005e6ac483f39c928de173bcdc26ab78813
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-12 21:11:54 +0200

    config hinzugefügt

---
+++ packages/backend/src/config.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import path from "path";
+import yaml from "js-yaml";
+import configSchema from "@schemas/config";
+
+// #region Types
+type Config = {
+    port: number;
+};
+// #endregion
+
+/**
+    Pfad zur Konfigurations-Datei.
+*/
+const CONFIG_PATH = path.resolve(__dirname, "../config/config.yml");
+
+const data = yaml.load(fs.readFileSync(CONFIG_PATH, "utf-8"));
+const config = data as Config;
+
+/**
+    Validiert die Konfigurations-Datei.
+*/
+const validateConfig = () => {
+    const { error } = configSchema.validate(data, {
+        presence: "required"
+    });
+    
+    if(error) {
+        const { message } = error.details[0];
+        throw new Error(`Fehlerhafte Konfiguration: ${message}`);
+    }
+};
+
+export {
+    validateConfig
+};
+
+export default config;
