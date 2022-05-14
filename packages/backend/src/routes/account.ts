5cadc2708553ccd33ac3a0e1bbe5e69cfa8df90e
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-15 00:45:29 +0200

    GET Methoden hinzugefügt

--- packages/backend/src/routes/account.ts
+++ packages/backend/src/routes/account.ts
@@ -6,6 +6,8 @@ import isLoggedIn from "@middleware/http/isLoggedIn";
 
 // Services
 import { verifyEmail } from "@services/account";
+import { fetchContacts } from "@services/contact";
+import { fetchNotifications } from "@services/notification";
 
 /**
     Router für Benutzerkonten.
@@ -13,6 +15,26 @@ import { verifyEmail } from "@services/account";
 const router = Router();
 
 router.use(isLoggedIn);
+
+// Abrufen der Kontakte
+router.get("/contacts", async (req: Request, res: Response) => {
+    try {
+        const contacts = await fetchContacts(res.locals.id);
+        sendData(res, { contacts });
+    } catch(error) {
+        sendError(res, error);
+    }
+});
+
+// Abrufen der Benachrichtungen
+router.get("/notifications", async (req: Request, res: Response) => {
+    try {
+        const notifications = await fetchNotifications(res.locals.id);
+        sendData(res, { notifications });
+    } catch(error) {
+        sendError(res, error);
+    }
+});
 
 // Bestätigung der E-Mail
 router.post("/verify-email", async (req: Request, res: Response) => {
