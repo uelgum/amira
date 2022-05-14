b1cf6a38279105f51ac0f6ace9323f5c1ff5bd36
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-14 20:22:31 +0200

    sendPresenceUpdate hinzugefügt

--- packages/backend/src/services/notification.ts
+++ packages/backend/src/services/notification.ts
@@ -1,6 +1,5 @@
 import sockets from "@controller/socket";
-import Notification from "@models/notification";
-import User from "@models/user";
+import Contact from "@models/contact";
 
 /**
     Sendet ein Update-Befehl für Benachrichtigungen an einen Nutzer.
@@ -13,6 +12,33 @@ const sendNotificationUpdate = async (id: string) => {
     socket.emit("notificationUpdate");
 };
 
+/**
+    Sendet ein Presence-Update an alle Kontakte eines Nutzers.
+*/
+const sendPresenceUpdate = async (id: string, status: string) => {
+    const contacts = await Contact.find({
+        $or: [
+            { contactId1: id },
+            { contactId2: id }
+        ]
+    });
+
+    for(const { contactId1, contactId2 } of contacts) {
+        // ID des Kontaktes finden
+        const contactId = (contactId1 === id) ? contactId2 : contactId1;
+
+        if(!sockets.has(contactId)) continue;
+
+        const socket = sockets.get(contactId);
+
+        socket.emit("presenceUpdate", {
+            id,
+            status
+        });
+    }
+};
+
 export {
-    sendNotificationUpdate
+    sendNotificationUpdate,
+    sendPresenceUpdate
 };
