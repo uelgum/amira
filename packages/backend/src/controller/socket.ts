0040c052250e3f39ccb0fb1c98daaa39e29474be
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-17 23:54:11 +0200

    Teimouts hinzugefügt

--- packages/backend/src/controller/socket.ts
+++ packages/backend/src/controller/socket.ts
@@ -1,8 +1,21 @@
 import type { Socket } from "socket.io";
 
+// #region Types
+type Timeout = ReturnType<typeof setTimeout>;
+// #endregion
+
 /**
     Kollektion aller verbundenen Sockets.
 */
 const sockets = new Map<string, Socket>();
+
+/**
+    Kollektion aller Timeouts für Presence-Updates.
+*/
+const timeouts = new Map<string, Timeout>();
+
+export {
+    timeouts
+};
 
 export default sockets;
