d5245f64eb217fe42f785698be613cbbba5630b3
wgumenyuk <wgumenyuk.gg@gmail.com>
2022-05-18 18:36:16 +0200

    Events überarbeitet

--- packages/backend/src/events/connection.ts
+++ packages/backend/src/events/connection.ts
@@ -1,39 +1,12 @@
-import sockets, { timeouts } from "@controller/socket";
-import { sendPresenceUpdate } from "@services/notification";
+import sockets from "@controller/socket";
 import type { Socket } from "socket.io";
-
-// Events
-import onDisconnect from "@events/disconnect";
 
 /**
     Handler für das `connection`-Event.
     Wird ausgeführt, sobald ein Socket eine Verbindung herstellt.
 */
 const onConnection = async (socket: Socket) => {
-    const id = socket.user.id;
-
-    if(sockets.has(id)) {
-        const oldSocket = sockets.get(id);
-        oldSocket.emit("multipleConnections");
-        oldSocket.disconnect();
-    }
-
-    // Events hinzufügen
-    socket.on("disconnect", onDisconnect.bind(null, socket));
-
-    sockets.set(id, socket);
-
-    // In Timeouts suchen
-    if(timeouts.has(id)) {
-        const timeout = timeouts.get(id);
-
-        clearTimeout(timeout);
-        timeouts.delete(id);
-        
-        return;
-    }
-
-    sendPresenceUpdate(id, "online");
+    sockets.add(socket);
 };
 
 export default onConnection;
--- packages/backend/src/events/disconnect.ts
+++ packages/backend/src/events/disconnect.ts
@@ -1,28 +1,12 @@
-import sockets, { timeouts } from "@controller/socket";
-import { sendPresenceUpdate } from "@services/notification";
+import sockets from "@controller/socket";
 import type { Socket } from "socket.io";
-
-/**
-    Zeit, nach der das Presence-Update gesendet wird.
-*/
-const PRESENCE_TIMEOUT = 1000 * 10;
 
 /**
     Handler für das `disconnect`-Event.
     Wird ausgeführt, sobald ein Socket eine Verbindung trennts.
 */
 const onDisconnect = async (socket: Socket) => {
-    const id = socket.user.id;
-
-    sockets.delete(id);
-
-    // Presence-Update debouncen
-    const timeout = setTimeout(() => {
-        sendPresenceUpdate(id, "offline");
-        timeouts.delete(id);
-    }, PRESENCE_TIMEOUT);
-
-    timeouts.set(id, timeout);
+    sockets.remove(socket);
 };
 
 export default onDisconnect;
