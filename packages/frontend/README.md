5b76f2ea20ae34b50d59e5a2d0d0ebd0cd599cc6
ar4ide <artjomgumenyuk08@gmail.com>
2022-05-21 18:40:46 +0200

    Web hinzugefügt

---
+++ packages/frontend/README.md
@@ -0,0 +1,6 @@
+# Frontend
+
+## TODO
+- Out-Directory zu `/build` ändern
+- Fonts hinzufügen
+- Ülgum Logo hinzufügen
---
+++ packages/frontend/src/App.svelte
@@ -0,0 +1,17 @@
+<h1>
+    Hello World!
+    <span></span>
+</h1>
+<span>Outside</span>
+
+<style lang="scss">
+    $c: red;
+
+    h1 {
+        color: $c;
+
+        span::before {
+            content: "Nesting!";
+        }
+    }
+</style>
