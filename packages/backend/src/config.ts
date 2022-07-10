717338a1774d89552e6ce5b08abb811ab9e5d3f1
wgumenyuk <wlad@gumenyuk.de>
2022-07-10 17:04:59 +0200

    Config hinzugefügt

---
+++ packages/backend/src/config.ts
@@ -0,0 +1,59 @@
+import fs from "fs";
+import path from "path";
+import yml from "js-yaml";
+
+// Intern
+import configSchema from "@schemas/config";
+
+// #region Types
+/**
+    Konfiguration.
+*/
+type Config = {
+    /**
+        Port des Servers.
+    */
+    port: number;
+
+    /**
+        JWT-Schlüssel.
+    */
+    jwtKey: string;
+
+    /**
+        Anmeldedaten für PostgreSQL.
+    */
+    psql: {
+        host: string;
+        username: string;
+        password: string;
+        db: string;
+    };
+};
+// #endregion
+
+/**
+    Pfad zur Konfigurationsdatei.
+*/
+const CONFIG_PATH = path.join(__dirname, "../config/config.yml");
+
+const file = fs.readFileSync(CONFIG_PATH, "utf-8");
+const config = yml.load(file) as Config;
+
+/**
+    Validiert die Konfigurationsdatei.
+*/
+const validateConfig = () => {
+    const { error } = configSchema.validate(config);
+
+    if(error) {
+        const { message } = error.details[0];
+        throw new Error(`Fehlerhafte Konfiguration: ${message}`);
+    }
+};
+
+export {
+    validateConfig
+};
+
+export default config;
