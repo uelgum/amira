c08431304862c6062a830236828a16c925041995
wgumenyuk <wlad@gumenyuk.de>
2022-07-11 21:15:20 +0200

    Contact-Model hinzugefügt

---
+++ packages/backend/src/models/contact.ts
@@ -0,0 +1,48 @@
+import { DataTypes, Model } from "sequelize";
+
+// Intern
+import sequelize from "@loaders/sequelize";
+
+// #region Types
+/**
+    Kontakt-Model in der Datenbank.
+*/
+type Contact = Model & {
+    /**
+        ID des ersten Partners.
+    */
+    id1: string;
+
+    /**
+        ID des zweiten Partners.
+    */
+    id2: string;
+
+    /**
+        Ob der Kontakt noch nicht bestätigt ist.
+    */
+    unconfirmed?: boolean;
+};
+// #endregion
+
+/**
+    Kontakt-Model in der Datenbank.
+*/
+const ContactModel = sequelize.define<Contact>(
+    "Contact",
+    {
+        id1: {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        id2: {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        unconfirmed: {
+            type: DataTypes.BOOLEAN
+        }
+    }
+);
+
+export default ContactModel;
