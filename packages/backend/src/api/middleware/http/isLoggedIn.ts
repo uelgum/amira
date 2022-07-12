df7a24fe27e4a916b5f96d3557f8b47b80821a4e
wgumenyuk <wlad@gumenyuk.de>
2022-07-12 09:17:20 +0200

    isLoggedIn hinzugefügt

---
+++ packages/backend/src/api/middleware/http/isLoggedIn.ts
@@ -0,0 +1,45 @@
+import jwt from "jwt-promisify";
+
+// Intern
+import AmiraError from "@structs/error";
+import { sendError } from "@utils/response";
+
+// Config
+import config from "@config";
+
+// Types
+import { Request, Response } from "express";
+
+// #region Types
+/**
+    Funktion, welche die nächste Middleware aufruft.
+*/
+type Next = () => void;
+// #endregion
+
+const isLoggedIn = async (req: Request, res: Response, next: Next) => {
+    const header = req.headers.authorization;
+
+    if(!header) {
+        sendError(res, new AmiraError(403, "UNAUTHORIZED"));
+        return;
+    }
+
+    const [ schema, token ] = header.split(" ");
+
+    if(!token || schema !== "Bearer") {
+        sendError(res, new AmiraError(403, "INVALID_TOKEN"));
+        return;
+    }
+
+    try {
+        req.user = await jwt.verify(token, config.jwtKey);
+    } catch(error) {
+        sendError(res, new AmiraError(403, "INVALID_TOKEN"));
+        return;
+    }
+
+    next();
+};
+
+export default isLoggedIn;
