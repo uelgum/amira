ce15feffa4879cd5674acb0b95a95eea3653605a
wgumenyuk <wlad@gumenyuk.de>
2022-07-12 19:52:06 +0200

    Register-Schema hinzugefügt

---
+++ packages/backend/src/schemas/register.ts
@@ -0,0 +1,35 @@
+import Joi from "joi";
+
+/**
+    Schema für Registrierungs-Daten.
+*/
+const registerSchema = Joi.object({
+    firstName: Joi.string()
+        .min(1)
+        .max(64),
+
+    lastName: Joi.string()
+        .min(1)
+        .max(64),
+
+    // Nutzername
+    username: Joi.string()
+        .min(1)
+        .max(16),
+
+    // E-Mail
+    email: Joi.string()
+        .email(),
+
+    // Passwort
+    password: Joi.string()
+        .min(8)
+        .max(32),
+
+    // Bestätigung des Passworts
+    passwordConfirm: Joi.string()
+        .min(8)
+        .max(32)
+});
+
+export default registerSchema;
