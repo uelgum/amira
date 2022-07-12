3c03195c5f8ad012333df502a66b016f5c0e4ac6
wgumenyuk <wlad@gumenyuk.de>
2022-07-12 09:09:17 +0200

    Response-Utils hinzugefügt

---
+++ packages/backend/src/utils/response.ts
@@ -0,0 +1,36 @@
+import AmiraError from "@structs/error";
+
+// Types
+import type { Response } from "express";
+
+/**
+    Schickt Daten an den Nutzer zurück.
+*/
+const sendData = (res: Response, data: Record<string, any>) => {
+    res.status(200);
+    res.json({
+        status: "ok",
+        data
+    });
+};
+
+/**
+    Schickt einen Fehler an den Nutzer zurück.
+*/
+const sendError = (res: Response, error: Error) => {
+    const status = (error instanceof AmiraError) ? error.status : 500;
+    const code = (error instanceof AmiraError) ? error.code : "INTERNAL_ERROR";
+
+    res.status(status);
+    res.json({
+        status: "err",
+        err: {
+            code
+        }
+    });
+};
+
+export {
+    sendData,
+    sendError
+};
