e836d7c8808ea43131ee5f67964c17878b570634
wgumenyuk <wlad@gumenyuk.de>
2022-07-14 16:49:57 +0200

    Verification-Model hinzugefügt

---
+++ packages/backend/src/models/verification.ts
@@ -0,0 +1,51 @@
+import { DataTypes, Model } from "sequelize";
+
+// Intern
+import sequelize from "@loaders/sequelize";
+
+// #region Types
+/**
+    Verification-Model in der Datenbank.
+*/
+type Verification = Model & {
+    /**
+        ID des Nutzers.
+    */
+    id: string;
+
+    /**
+        Bestätigung-ID.
+    */
+    verificationId: string;
+
+    /**
+        Erstelldatum der Bestätigung.
+    */
+    createdAt: number;
+};
+// #endregion
+
+/**
+    Kontakt-Model in der Datenbank.
+*/
+const VerificationModel = sequelize.define<Verification>(
+    "Verification",
+    {
+        id: {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        verificationId: {
+            type: DataTypes.STRING(64),
+            allowNull: false,
+            field: "verification_id"
+        },
+        createdAt: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            field: "created_at"
+        }
+    }
+);
+
+export default VerificationModel;
