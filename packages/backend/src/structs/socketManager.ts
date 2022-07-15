0b62dc82477bc1f1623c812e2fb02a8bc4896c79
wgumenyuk <wlad@gumenyuk.de>
2022-07-15 23:21:36 +0200

    Socket-Manager hinzugefügt

---
+++ packages/backend/src/structs/socketManager.ts
@@ -0,0 +1,71 @@
+import type { Socket } from "socket.io";
+
+/**
+    Manager für Socket-Verbindungen.
+*/
+class SocketManager {
+    // #region Attribute
+    /**
+        Kollektion aller verbundenen Sockets.
+    */
+    private sockets: Map<string, Socket>;
+
+    /**
+        Kollektion aller aktiven Debounces.
+    */
+    public debounces: Map<string, NodeJS.Timeout>;
+    // #endregion
+
+    /**
+        Konstruktor von `SocketManager`.
+    */
+    constructor() {
+        this.sockets = new Map();
+        this.debounces = new Map();
+    }
+
+    /**
+        Anzahl der verbundenen Sockets.
+    */
+    public get size() {
+        return this.sockets.size;
+    }
+
+    /**
+        Fügt einen Socket zum Manager hinzu.
+    */
+    public add(socket: Socket) {
+        const socketId = socket.user.id;
+
+        if(this.sockets.has(socketId)) {
+            const oldSocket = this.sockets.get(socketId)!;
+
+            oldSocket.emit("multipleConnections");
+            oldSocket.disconnect();
+        }
+
+        // TODO Events hinzufügen
+
+        this.sockets.set(socketId, socket);
+    }
+
+    /**
+        Entfernt einen Socket aus dem Manager.
+    */
+    public remove(socket: Socket) {
+        if(socket.connected) {
+            socket.disconnect();
+        }
+
+        this.sockets.delete(socket.user.id);
+    }
+
+    /**
+        Schickt ein Event an alle verbundenen Sockets.
+    */
+    public broadcast(event: string, data: Record<string, any>) {
+        this.sockets.forEach((socket) => socket.emit(event, data));
+    }
+}
+
+export default SocketManager;
