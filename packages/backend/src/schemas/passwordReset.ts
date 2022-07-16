032da74b4b651d71829db09a60b4595efd3866a1
wgumenyuk <wlad@gumenyuk.de>
2022-07-16 16:32:40 +0200

    Schema für Passwort-Resets hinzugefügt

---
+++ packages/backend/src/schemas/passwordReset.ts
@@ -0,0 +1,29 @@
+import Joi from "joi";
+
+// Intern
+import { PASSWORD_REGEX } from "@schemas/register";
+
+/**
+    Schema für Passwort-Reset-Daten.
+*/
+const passwordResetSchema = Joi.object({
+    // Action-ID
+    actionId: Joi.string(),
+
+    recoveryCode: Joi.string()
+        .length(19),
+
+    // Neues Passwort
+    newPassword: Joi.string()
+        .min(8)
+        .max(32)
+        .regex(PASSWORD_REGEX),
+
+    // Bestätigung des neuen Passworts
+    newPasswordConfirm: Joi.string()
+        .min(8)
+        .max(32)
+        .regex(PASSWORD_REGEX)
+});
+
+export default passwordResetSchema;
