74caf0f407fa430d7d5ca16113fab6d409a7cb1d
wgumenyuk <wlad@gumenyuk.de>
2022-07-16 16:33:11 +0200

    resetPassword hinzugefügt

--- packages/backend/src/services/user.ts
+++ packages/backend/src/services/user.ts
@@ -1,6 +1,8 @@
 import AmiraError from "@structs/error";
 import User from "@models/user";
 import Email from "@models/email";
+import { validatePasswordResetData } from "@services/validator";
+import { decrypt, derivePasswordKey, encrypt, generateRecoveryKey, hashPassword } from "@services/crypto";
 
 /**
     Verifiziert die E-Mail eines Nutzers.
@@ -38,6 +40,68 @@ const verifyEmail = async (data: Record<string, any>) => {
     await verification.destroy();
 };
 
+/**
+    Setzt das Passwort zurück.
+*/
+const resetPassword = async (data: Record<string, any>) => {
+    const isValid = validatePasswordResetData(data);
+
+    if(!isValid) {
+        throw new AmiraError(400, "INVALID_DATA");
+    }
+
+    const { actionId, recoveryCode, newPassword, newPasswordConfirm } = data;
+
+    const reset = await Email.findOne({
+        where: {
+            actionId
+        }
+    });
+
+    if(!reset) {
+        throw new AmiraError(400, "INVALID_DATA");
+    }
+
+    const user = await User.findOne({
+        where: {
+            id: reset.userId
+        }
+    });
+
+    if(!user) {
+        throw new AmiraError(400, "USER_NOT_FOUND");
+    }
+
+    if(newPassword !== newPasswordConfirm) {
+        throw new AmiraError(400, "MISMATCHED_PASSWORDS");
+    }
+
+    const oldPasswordKey = decrypt(user.recoveryKey, recoveryCode);
+
+    if(oldPasswordKey.length === 0) {
+        throw new AmiraError(400, "INVALID_RECOVERY_CODE");
+    }
+
+    const newPasswordHash = await hashPassword(newPassword);
+    const newPasswordKey = derivePasswordKey(newPassword, user.createdAt);
+
+    // Neuen Reovery-Code und -Key generieren
+    const [ newRecoveryCode, newRecoveryKey ] = generateRecoveryKey(newPasswordKey);
+
+    // User-Key mit altem Passwort-Key entschlüsseln und mit neuem Passwort-Key verschlüsseln
+    const userKey = encrypt(
+        decrypt(user.userKey, oldPasswordKey),
+        newPasswordKey
+    );
+
+    user.password = newPasswordHash;
+    user.recoveryKey = newRecoveryKey;
+    user.userKey = userKey;
+
+    return newRecoveryCode;
+};
+
 export {
-    verifyEmail
+    verifyEmail,
+    resetPassword
 };
