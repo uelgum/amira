f661bea1a608c412a97dd623cb5f594cf171cae0
wgumenyuk <wlad@gumenyuk.de>
2022-07-20 13:33:43 +0200

    Fix: Kommentar

--- packages/backend/src/utils/exists.ts
+++ packages/backend/src/utils/exists.ts
@@ -1,5 +1,8 @@
 import type { Model, ModelStatic, WhereOptions } from "sequelize";
 
+/**
+    Überprüft, ob ein Eintrag in der Datenbank existiert.
+*/
 const exists = async <T extends Model>(model: ModelStatic<T>, where: WhereOptions<T>) => {
     const count = await model.count({
         where
