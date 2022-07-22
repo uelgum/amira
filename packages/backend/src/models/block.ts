319156799fed6de7a3865e266073ac133f62005c
wgumenyuk <wlad@gumenyuk.de>
2022-07-22 23:16:00 +0200

    Block-Model erstellt

---
+++ packages/backend/src/models/block.ts
@@ -0,0 +1,62 @@
+import { DataTypes, Model } from "sequelize";
+
+// Intern
+import sequelize from "@loaders/sequelize";
+
+// #region Types
+/**
+    Block-Model in der Datenbank.
+*/
+type BlockModel = Model & {
+    /**
+        ID des Blocks.
+    */
+    id: string;
+
+    /**
+        ID des Nutzers.
+    */
+    userId: string;
+
+    /**
+        ID des geblockten Nutzers.
+    */
+    blockedUserId: string;
+
+    /**
+        Erstelldatum des Block.
+    */
+    createdAt: number;
+};
+// #endregion
+
+/**
+    Block-Model in der Datenbank.
+*/
+const BlockModel = sequelize.define<BlockModel>(
+    "Block",
+    {
+        id: {
+            type: DataTypes.STRING(20),
+            primaryKey: true,
+            allowNull: false,
+            unique: true
+        },
+        userId: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            field: "user_id"
+        },
+        blockedUserId: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            field: "blocked_user_id"
+        },
+        createdAt: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            field: "created_at"
+        }
+    }
+);
+export default BlockModel;
