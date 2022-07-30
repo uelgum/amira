211a18971ae0eff2872225ebce1666ba04bcc11c
wgumenyuk <wlad@gumenyuk.de>
2022-07-30 08:47:40 +0200

    Register-Schema erweitert

--- packages/backend/src/schemas/register.ts
+++ packages/backend/src/schemas/register.ts
@@ -6,6 +6,11 @@ import Joi from "joi";
 const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
 /**
+    Regex für Public-Keys (OpenPGP).
+*/
+const PUBLIC_KEY_REGEX = /^-----BEGIN PGP PUBLIC KEY BLOCK-----[a-zA-Z0-9/+=\n]+-----END PGP PUBLIC KEY BLOCK-----$/;
+
+/**
     Schema für Registrierungs-Daten.
 */
 const registerSchema = Joi.object({
@@ -38,7 +43,11 @@ const registerSchema = Joi.object({
     passwordConfirm: Joi.string()
         .min(8)
         .max(32)
-        .regex(PASSWORD_REGEX)
+        .regex(PASSWORD_REGEX),
+    
+    // Public-Key (OpenPGP)
+    publicKey: Joi.string()
+        .regex(PUBLIC_KEY_REGEX)
 });
 
 export {
