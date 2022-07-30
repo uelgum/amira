25241de4d0d285e35c8c566c6a34c94c26134c2a
wgumenyuk <wlad@gumenyuk.de>
2022-07-30 10:14:04 +0200

    Kommentar erweitert

--- packages/backend/src/api/events/disconnect.ts
+++ packages/backend/src/api/events/disconnect.ts
@@ -11,7 +11,8 @@ const DEBOUNCE_DURATION = 1000 * 10;
 
 /**
     Handler für das `disconnect`-Event.
-    Wird ausgeführt, sobald ein Socket die Verbindung trennt.
+    Wird ausgeführt, sobald ein Socket die Verbindung trennt. Der Nutzer hat 10 Sekunden
+    Zeit, bevor dieser als offline angezeigt wird.
 */
 const onDisconnect = (socketManager: SocketManager, socket: Socket) => {
     const socketId = socket.user.id;
