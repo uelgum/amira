793d48d40dea8c033e7a655804b80bda0ee9152a
wgumenyuk <wlad@gumenyuk.de>
2022-07-31 21:06:14 +0200

    Indent: 2 -> 4

--- packages/frontend/src-tauri/src/main.rs
+++ packages/frontend/src-tauri/src/main.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("Amira konnte nicht gestartet werden.");
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("Amira konnte nicht gestartet werden.");
 }
