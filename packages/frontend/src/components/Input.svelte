614255cd307166dbb8c0327a88e4da780bca4576
wgumenyuk <wlad@gumenyuk.de>
2022-08-05 21:10:22 +0200

    Input hinzugefügt

---
+++ packages/frontend/src/components/Input.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let id;
+    export let type: "text" | "password" = "text";
+    export let placeholder = "";
+</script>
+
+<input
+    id={id}
+    class="input"
+    type={type}
+    spellcheck="false"
+    placeholder={placeholder}
+/>
+
+<style lang="scss">
+    @import "../scss/variables";
+    
+    .input {
+        background: $BLACK;
+        color: $WHITE_090;
+        padding: 1em;
+        border: 1px solid $GRAY;
+        border-radius: 0.5em;
+        outline: 0px;
+    }
+</style>
