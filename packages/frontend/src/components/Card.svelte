9040210a7b6386b95c0e9638cb7e0c73d5532db0
wgumenyuk <wlad@gumenyuk.de>
2022-08-05 21:09:49 +0200

    Card hinzugefügt

---
+++ packages/frontend/src/components/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let id = "";
+</script>
+
+<div id={id} class="card">
+    <slot/>
+</div>
+
+<style lang="scss">
+    @import "../scss/variables";
+    
+    .card {
+        display: inline-block;
+        position: relative;
+        background: $BLACKER;
+        color: $WHITE_090;
+        padding: 1.5em;
+        border: 1px solid $GRAY;
+        border-radius: 0.5em;
+    }
+</style>
