178d1b3d9bea30d0ba62b882aa34325c27306e00
wgumenyuk <wlad@gumenyuk.de>
2022-08-07 16:33:15 +0200

    Card -> flex

--- packages/frontend/src/components/Card.svelte
+++ packages/frontend/src/components/Card.svelte
@@ -10,12 +10,13 @@
     @import "../scss/variables";
     
     .card {
-        display: inline-block;
+        display: flex;
         position: relative;
         background: $BLACKER;
         color: $WHITE_090;
         padding: 1.5em;
         border: 1px solid $GRAY;
         border-radius: 0.5em;
+        flex-direction: column;
     }
 </style>
