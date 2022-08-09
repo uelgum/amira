92f45bd3dfa03f103c96abf1e70dc7544563beee
wgumenyuk <wlad@gumenyuk.de>
2022-08-09 16:40:41 +0200

    Button hinzugefügt

---
+++ packages/frontend/src/components/Button.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let id = "";
+</script>
+
+<button id={id} on:click>
+    <slot/>
+</button>
+
+<style lang="scss">
+    @import "../scss/variables";
+    
+    button {
+        background: $PURPLE_080;
+        color: $WHITE_090;
+        width: 100%;
+        padding: 1em;
+        border: 1px solid $GRAY;
+        border-radius: 0.5em;
+        outline: 0px;
+        font-family: "Inter", sans-serif;
+        font-size: 1em;
+        letter-spacing: 0.025em;
+        cursor: pointer;
+
+        &:hover {
+            background: $PURPLE;
+        }
+    }
+</style>
