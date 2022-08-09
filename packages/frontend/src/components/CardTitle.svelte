b8c302b6f826661b4f98a36efec310575341f3b4
wgumenyuk <wlad@gumenyuk.de>
2022-08-09 16:41:46 +0200

    CardTitle verbessert

--- packages/frontend/src/components/CardTitle.svelte
+++ packages/frontend/src/components/CardTitle.svelte
@@ -3,7 +3,11 @@
 </span>
 
 <style lang="scss">
+    @import "../scss/variables";
+
     .card-title {
+        color: $WHITE_100;
+        padding-bottom: 1em;
         font-weight: 600;
         user-select: none;
     }
