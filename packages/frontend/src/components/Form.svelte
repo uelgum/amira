15ff52ba0f502789c8f5fe21e0dd16f3a3b1df94
wgumenyuk <wlad@gumenyuk.de>
2022-08-09 16:41:10 +0200

    Form hinzugefügt

---
+++ packages/frontend/src/components/Form.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    const handleSubmit = (event: Event) => {
+        event.preventDefault();
+    };
+</script>
+
+<form on:submit={handleSubmit} autocomplete="off" spellcheck="false">
+    <slot/>
+</form>
