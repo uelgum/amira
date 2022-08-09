9416c73f186a1e8e4a0d85dc90c2711850eafca6
wgumenyuk <wlad@gumenyuk.de>
2022-08-09 21:16:56 +0200

    Tab hinzugefügt

---
+++ packages/frontend/src/components/Tab.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { fly } from "svelte/transition";
+
+    // Types
+    import type { FlyParams } from "svelte/transition";
+
+    const flyIn: FlyParams = {
+        x: -window.innerWidth,
+        delay: 350
+    };
+
+    const flyOut: FlyParams = {
+        x: window.innerWidth,
+        duration: 350
+    };
+</script>
+
+<div class="tab" in:fly={flyIn} out:fly={flyOut}>
+    <slot/>
+</div>
+
+<style lang="scss">
+    .tab {
+        width: 100%;
+        height: 100%;
+    }
+</style>
