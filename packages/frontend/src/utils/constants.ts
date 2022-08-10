74972366aa140415eb45a7c432bd6d89568562ee
wgumenyuk <wlad@gumenyuk.de>
2022-08-10 15:25:32 +0200

    Regex für E-Mail und Passwort hinzugefügt

--- packages/frontend/src/utils/constants.ts
+++ packages/frontend/src/utils/constants.ts
@@ -2,3 +2,13 @@
     Schlüssel im `LocalStorage` für JWTs.
 */
 export const TOKEN_KEY = "TOKEN";
+
+/**
+    Regex für E-Mail-Adressen.
+*/
+export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+/**
+    Regex für Passwörter.
+*/
+export const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
