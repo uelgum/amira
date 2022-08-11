3fac38498e8e176cf539d773ca3087cadf6bf471
wgumenyuk <wlad@gumenyuk.de>
2022-08-11 22:23:32 +0200

    Desktop-Dateien hinzugefügt

---
+++ packages/frontend/desktop/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    
+</script>
+
+<main>
+
+</main>
+
+<style lang="scss">
+    main {
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ packages/frontend/desktop/vite.config.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
+
+export default defineConfig({
+    plugins: [
+        svelte({
+            preprocess: sveltePreprocess()
+        })
+    ],
+    build: {
+        outDir: "build",
+        assetsDir: "public"
+    },
+    resolve: {
+        alias: [
+            { find: "@components", replacement: path.join(__dirname, "src/components") }
+        ]
+    }
+});
