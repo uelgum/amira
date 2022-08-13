44489a8733bcccc0e66446058b8f299edc6a7876
wgumenyuk <wlad@gumenyuk.de>
2022-08-13 16:07:03 +0200

    TitleBar hinzugefügt

---
+++ packages/frontend/desktop/src/components/TitleBar/TitleBarName.svelte
@@ -0,0 +1,13 @@
+<div class="titlebar-name">
+    <span>Amira</span>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .titlebar-name {
+        color: $white-lighter;
+        padding-left: 0.5em;
+        font-size: 0.75em;
+    }
+</style>
