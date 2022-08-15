380d03cf2d8c0b24410f8a945ecbdc5ee31e88e8
wgumenyuk <wlad@gumenyuk.de>
2022-08-15 22:22:57 +0200

    Card hinzugefügt

---
+++ packages/frontend/shared/components/Card/Card.svelte
@@ -0,0 +1,15 @@
+<div class="card">
+    <slot/>
+</div>
+
+<style lang="scss">
+    @use "../../scss/colors" as *;
+
+    .card {
+        background: $black-light;
+        max-width: 40em;
+        padding: 2em;
+        border: 1px solid $black-lightest;
+        border-radius: 0.5em;
+    }
+</style>
