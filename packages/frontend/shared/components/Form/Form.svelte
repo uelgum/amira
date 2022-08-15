71a5f2fd69d01dd080ab26b8b7f3c760c4121894
wgumenyuk <wlad@gumenyuk.de>
2022-08-15 22:23:41 +0200

    Form hinzugefügt

---
+++ packages/frontend/shared/components/Form/Form.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    const handleSubmit = (event: Event) => {
+        event.preventDefault();
+    };
+</script>
+
+<!--
+    ⚠️ Viele Browser ignorieren "autocomplete", dies kann jedoch mit einem
+    undefinierten String umgangen werden. Auf diese Weise wird Autocomplete
+    deaktiviert. 
+-->
+
+<form class="form" spellcheck="false" autocomplete="autocomplete-amira" on:submit={handleSubmit}>
+    <slot/>
+</form>
+
+<style lang="scss">
+    .form {
+        display: flex;
+        flex-direction: column;
+        gap: 2em;
+    }
+</style>
