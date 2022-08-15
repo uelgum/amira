457164da5996adeb50f67c94cd553b9205dabc69
wgumenyuk <wlad@gumenyuk.de>
2022-08-15 22:23:34 +0200

    Col hinzugefügt

---
+++ packages/frontend/shared/components/Col/Col.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let xs = 12;
+    export let sm = 12;
+    export let md = 12;
+    export let lg = 12;
+
+    const props = {
+        xs, sm, md, lg
+    };
+
+    const className = Object
+        .entries(props)
+        .map(([ name, size ]) => `col-${name}-${size}`)
+        .join(" ");
+</script>
+
+<div class={className}>
+    <slot/>
+</div>
