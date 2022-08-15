01e38c82d3cb9ab6e9cc69ccd7de01a6c4cc4897
wgumenyuk <wlad@gumenyuk.de>
2022-08-15 22:24:16 +0200

    Label hinzugefügt

---
+++ packages/frontend/shared/components/Label/Label.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let forId: string;
+
+    export { forId as for };
+</script>
+
+<label class="label" for={forId}>
+    <slot/>
+</label>
+
+<style lang="scss">
+    @use "../../scss/colors" as *;
+    
+    .label {
+        color: $white-lighter;
+        user-select: none;
+    }
+</style>
