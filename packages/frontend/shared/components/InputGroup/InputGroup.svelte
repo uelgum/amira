5a828fc842eadbff561ae9d741f4eb899c04ed76
wgumenyuk <wlad@gumenyuk.de>
2022-08-15 22:24:08 +0200

    InputGroup hinzugefügt

---
+++ packages/frontend/shared/components/InputGroup/InputGroup.svelte
@@ -0,0 +1,11 @@
+<div class="input-group">
+    <slot/>
+</div>
+
+<style lang="scss">
+    .input-group {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5em;
+    }
+</style>
