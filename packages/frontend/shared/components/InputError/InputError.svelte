e57d2b3d8bf7c0b0fa4173ab2bc729090dd0265e
wgumenyuk <wlad@gumenyuk.de>
2022-08-15 22:23:58 +0200

    InputError hinzugefügt

---
+++ packages/frontend/shared/components/InputError/InputError.svelte
@@ -0,0 +1,14 @@
+<span class="input-error">
+    <slot/>
+</span>
+
+<style lang="scss">
+    @use "../../scss/colors" as *;
+    
+    .input-error {
+        color: $red;
+        font-size: 0.85em;
+        font-weight: 600;
+        user-select: none;
+    }
+</style>
