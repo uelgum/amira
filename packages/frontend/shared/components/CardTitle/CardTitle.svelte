378801ccd033e9e8f3c10affb3da93674f1d7a8d
wgumenyuk <wlad@gumenyuk.de>
2022-08-15 22:23:15 +0200

    CardTitle hinzugefügt

---
+++ packages/frontend/shared/components/CardTitle/CardTitle.svelte
@@ -0,0 +1,15 @@
+<span class="card-title">
+    <slot/>
+</span>
+
+<style lang="scss">
+    @use "../../scss/colors" as *;
+
+    .card-title {
+        display: block;
+        color: $white;
+        margin-bottom: 2em;
+        font-weight: 600;
+        user-select: none;
+    }
+</style>
