606ee457e0551c834838a0db96088f59f92ecdeb
wgumenyuk <wlad@gumenyuk.de>
2022-08-16 12:38:54 +0200

    CardContent hinzugefügt

---
+++ packages/frontend/shared/components/CardContent/CardContent.svelte
@@ -0,0 +1,11 @@
+<div class="card-content">
+    <slot/>
+</div>
+
+<style lang="scss">
+    .card-content {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+    }
+</style>
