fe86171964d899fd90dbc734d17f31a3ae2bc2e5
wgumenyuk <wlad@gumenyuk.de>
2022-08-16 12:39:05 +0200

    CardText hinzugefügt

---
+++ packages/frontend/shared/components/CardText/CardText.svelte
@@ -0,0 +1,12 @@
+<p class="card-text">
+    <slot/>
+</p>
+
+<style lang="scss">
+    @use "../../scss/colors" as *;
+
+    .card-text {
+        color: $white-light;
+        user-select: none;
+    }
+</style>
