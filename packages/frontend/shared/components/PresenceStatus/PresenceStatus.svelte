d9224a987fe696adc3f0e31ede4b480abc089bdb
wgumenyuk <wlad@gumenyuk.de>
2022-08-23 21:03:29 +0200

    PresenceStatus hinzugefügt

---
+++ packages/frontend/shared/components/PresenceStatus/PresenceStatus.svelte
@@ -0,0 +1,38 @@
+<script context="module" lang="ts">
+    // #region Types
+    export type PresenceStatusType = "online" | "away" | "dnd" | "offline";
+    // #endregion
+</script>
+
+<script lang="ts">
+    export let type: PresenceStatusType;
+</script>
+
+<div class="presence-status {type}"></div>
+
+<style lang="scss">
+    @use "../../scss/colors" as *;
+
+    .presence-status {
+        width: 0.85em;
+        height: 0.85em;
+        border: 1px solid $black-lighter;
+        border-radius: 50%;
+
+        &.online {
+            background: $green;
+        }
+
+        &.away {
+            background: $yellow;
+        }
+
+        &.dnd {
+            background: $red;
+        }
+
+        &.offline {
+            background: lighten($black-lightest, 15);
+        }
+    }
+</style>
---
+++ packages/frontend/shared/components/PresenceStatus/index.ts
@@ -0,0 +1,1 @@
+export { default } from "./PresenceStatus.svelte";
