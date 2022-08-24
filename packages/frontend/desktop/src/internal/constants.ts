c73973917fe3a2e22b5610228763c5a2d9abe5cf
wgumenyuk <wlad@gumenyuk.de>
2022-08-24 18:37:37 +0200

    Key für Presence hinugefügt

--- packages/frontend/desktop/src/internal/constants.ts
+++ packages/frontend/desktop/src/internal/constants.ts
@@ -2,3 +2,8 @@
     Schlüssel für Token im `LocalStorage`.
 */
 export const TOKEN_KEY = "TOKEN";
+
+/**
+    Schlüssel für den Presence-Status im `LocalStorage`.
+*/
+export const PRESENCE_KEY = "PRESENCE";
