7339552ad0beed41da019f68a10313c06cbda5cc
wgumenyuk <wlad@gumenyuk.de>
2022-08-26 12:27:02 +0200

    Time-Readable hinzugefügt

---
+++ packages/frontend/desktop/src/stores/time.ts
@@ -0,0 +1,22 @@
+import { readable } from "svelte/store";
+
+/**
+    Intervall, in dem das Datum und die Uhrzeit aktualsiert werden.
+    Beträgt `10s`.
+*/
+const DATE_INTERVAL = 1000 * 10;
+
+/**
+    Readable Store für das aktuelle Datum und die Uhrzeit.
+*/
+const date = readable(new Date(), (set) => {
+    const interval = setInterval(() => {
+        set(new Date());
+    }, DATE_INTERVAL);
+
+    return () => {
+        clearInterval(interval);
+    };
+});
+
+export default date;
