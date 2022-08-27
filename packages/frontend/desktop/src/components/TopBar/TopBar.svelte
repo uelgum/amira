48a5114653078ce1932d56f1dc5763e417f358c7
wgumenyuk <wlad@gumenyuk.de>
2022-08-27 12:45:23 +0200

    TopBar hinzugefügt

---
+++ packages/frontend/desktop/src/components/TopBar/TopBar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import CurrentUser from "@components/CurrentUser";
+    import SideBarToggle from "@components/SideBarToggle";
+</script>
+
+<div class="topbar">
+    <SideBarToggle/>
+    <CurrentUser/>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+    
+    .topbar {
+        display: flex;
+        position: relative;
+        background: $black-light;
+        width: 100%;
+        height: 3em;
+        padding: 0em 0.5em 0em 0.5em;
+        border: 1px solid $black-lightest;
+        border-left: none;
+        border-right: none;
+        justify-content: space-between;
+        align-items: center;
+        user-select: none;
+    }
+</style>
