7c967e0f47e02ed05cfcdd6651ae97f74c6edcc1
wgumenyuk <wlad@gumenyuk.de>
2022-08-27 12:45:58 +0200

    DashboardTitle hinzugefügt

---
+++ packages/frontend/desktop/src/components/DashboardTitle/DashboardTitle.svelte
@@ -0,0 +1,14 @@
+<h1 class="dashboard-title">
+    <slot/>
+</h1>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .dashboard-title {
+        padding-bottom: 0.25em;
+        font-size: 1.25em;
+        font-weight: 400;
+        user-select: none;
+    }
+</style>
