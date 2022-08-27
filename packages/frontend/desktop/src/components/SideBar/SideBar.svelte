bde0f616a45fc27ffcf9c035ef6314cd8881f626
wgumenyuk <wlad@gumenyuk.de>
2022-08-27 12:45:33 +0200

    SideBar hinzugefügt

---
+++ packages/frontend/desktop/src/components/SideBar/SideBar.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+    import minimized from "@stores/sideBar";
+
+    // Icons
+    import homeIcon from "@amira/shared/svg/home.svg";
+    import mailIcon from "@amira/shared/svg/mail.svg";
+
+    /**
+        Überprüft, ob ein Link aktiv ist.
+    */
+    const isActive = (path: string) => {
+        return (window.location.pathname === path);
+    };
+</script>
+
+<div class="sidebar" class:sidebar-minimized={$minimized}>
+    <!-- Dashboard -->
+    <a class="sidebar-item" class:active={isActive("/dashboard")} href="/dashboard">
+        <img
+            src={homeIcon}
+            draggable="false"
+            alt="home-icon"
+        />
+        <span class="sidebar-item-label">Dashboard</span>
+        <span class="sidebar-item-tooltip">Dashboard</span>
+    </a>
+
+    <span class="sidebar-seperator"/>
+
+    <!-- Mails -->
+    <a class="sidebar-item" class:active={isActive("/mails")} href="/mails">
+        <img
+            src={mailIcon}
+            draggable="false"
+            alt="mail-icon"
+        />
+        <span class="sidebar-item-label">Nachrichten</span>
+        <span class="sidebar-item-tooltip">Nachrichten</span>
+    </a>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .sidebar {
+        position: relative;
+        background: $black-light;
+        width: 15%;
+        max-width: 10em;
+        padding: 0.5em;
+        border-right: 1px solid $black-lightest;
+        transition: all 0.25s;
+    }
+
+    .sidebar-minimized {
+        width: 3.5em;
+
+        .sidebar-item-label {
+            display: none;
+        }
+
+        .sidebar-item:hover > .sidebar-item-tooltip {
+            opacity: 1;
+        }
+    }
+
+    .sidebar-item-tooltip {
+        position: absolute;
+        background: $black-light;
+        left: calc(100% + 1em);
+        padding: 0.5em;
+        border: 1px solid $black-lightest;
+        border-radius: 0.5em;
+        pointer-events: none;
+        opacity: 0;
+        transition: opacity 0.25s;
+    }
+
+    .sidebar-item {
+        display: flex;
+        color: $white-light;
+        padding: 0.5em;
+        border-radius: 0.5em;
+        align-items: center;
+        gap: 0.5em;
+        font-size: 0.85em;
+        user-select: none;
+        text-decoration: none;
+        transition: all 0.25s;
+
+        &:not(:last-of-type) {
+            margin-bottom: 0.5em;
+        }
+
+        &:hover {
+            background: $black-lighter;
+        }
+
+        &:hover:first-of-type {
+            background: $blue;
+        }
+
+        &.active {
+            background: $black-lighter;
+        }
+
+        &.active:first-of-type {
+            background: $blue;
+        }
+    }
+
+    .sidebar-seperator {
+        display: block;
+        width: 100%;
+        margin-bottom: 0.5em;
+        border-bottom: 1px solid $black-lightest;
+    }
+</style>
