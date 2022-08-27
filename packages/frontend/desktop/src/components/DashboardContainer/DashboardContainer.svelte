b448b6e96bbae55721157fd9c5f4d58258a4c5be
wgumenyuk <wlad@gumenyuk.de>
2022-08-27 12:46:10 +0200

    DashboardContainer hinzugefügt

---
+++ packages/frontend/desktop/src/components/DashboardContainer/DashboardContainer.svelte
@@ -0,0 +1,11 @@
+<div class="dashboard-container">
+    <slot/>
+</div>
+
+<style lang="scss">
+    .dashboard-container {
+        display: flex;
+        width: 100%;
+        height: 100%;
+    }
+</style>
