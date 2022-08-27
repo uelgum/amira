cc5bdbe800b32209d877bb40718c7c38c8c9424b
wgumenyuk <wlad@gumenyuk.de>
2022-08-27 12:46:35 +0200

    Dashboard hinzugefügt

---
+++ packages/frontend/desktop/src/components/Dashboard/Dashboard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import TopBar from "@components/TopBar";
+    import SideBar from "@components/SideBar";
+    import DashboardContainer from "@components/DashboardContainer";
+    import DashboardContent from "@components/DashboardContent";
+</script>
+
+<TopBar/>
+<DashboardContainer>
+    <SideBar/>
+    <DashboardContent>
+        <slot/>
+    </DashboardContent>
+</DashboardContainer>
