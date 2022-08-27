a7affb23ad70f1cdf0c04df256290f0c01181bba
wgumenyuk <wlad@gumenyuk.de>
2022-08-27 12:44:57 +0200

    SideBarToggle hinzugefügt

---
+++ packages/frontend/desktop/src/components/SideBarToggle/SideBarToggle.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+import type sideBar from "@stores/sideBar";
+
+
+    import sideBar from "@stores/sideBar";
+
+    // Icons
+    import menuIcon from "@amira/shared/svg/menu.svg";
+
+    /**
+        Schaltet die Side-Bar um.
+    */
+    const toggleSideBar = () => {
+        if($sideBar) {
+            $sideBar = false;
+        } else {
+            $sideBar = true;
+        }
+    };
+</script>
+
+<button class="sidebar-toggle" on:click={toggleSideBar}>
+    <img
+        src={menuIcon}
+        draggable="false"
+        alt="menu-icon"
+    />
+</button>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .sidebar-toggle {
+        background: $black-light;
+        padding: 0.25em 0.5em 0.25em 0.5em;
+        border: none;
+        border-radius: 0.5em;
+        outline: none;
+        cursor: pointer;
+
+        &:hover {
+            background: $black-lighter;
+        }
+    }
+</style>
