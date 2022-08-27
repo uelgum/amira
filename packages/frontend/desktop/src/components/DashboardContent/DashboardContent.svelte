e6c11ae116222e161503b58326ed8d3537a2784f
wgumenyuk <wlad@gumenyuk.de>
2022-08-27 12:46:26 +0200

    DashboardContent hinzugefügt

---
+++ packages/frontend/desktop/src/components/DashboardContent/DashboardContent.svelte
@@ -0,0 +1,11 @@
+<div class="dashboard-content">
+    <slot/>
+</div>
+
+<style lang="scss">
+    .dashboard-content {
+        width: 100%;
+        height: 100%;
+        padding: 2em;
+    }
+</style>
