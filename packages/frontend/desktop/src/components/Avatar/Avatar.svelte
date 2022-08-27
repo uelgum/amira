5043c04230bdbe5f01742cd31f3c8c5d6b49be00
wgumenyuk <wlad@gumenyuk.de>
2022-08-27 12:44:24 +0200

    Avatar hinzugefügt

---
+++ packages/frontend/desktop/src/components/Avatar/Avatar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import presence from "@stores/presence";
+
+    // Komponente
+    import PresenceStatus from "@amira/shared/components/PresenceStatus";
+
+    export let width = "2.25em";
+    export let height = "2.25em";
+</script>
+
+<div class="avatar" style="--width: {width}; --height: {height}">
+    <div class="presence-status-container">
+        <PresenceStatus type={$presence}/>
+    </div>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+    
+    .avatar {
+        position: relative;
+        background: $black-lightest;
+        width: var(--width);
+        height: var(--height);
+        border-radius: 50%;
+
+        .presence-status-container {
+            position: absolute;
+            right: -0.15em;
+            bottom: 0em;
+        }
+    }
+</style>
