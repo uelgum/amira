a60e9e008e6d7decaca64a097ee735640e86eeb0
wgumenyuk <wlad@gumenyuk.de>
2022-08-28 19:00:06 +0200

    Mail-Model hinzugefügt

---
+++ packages/backend/src/models/apps/mail.ts
@@ -0,0 +1,72 @@
+import { DataTypes, Model } from "sequelize";
+
+// Intern
+import sequelize from "@loaders/sequelize";
+
+// #region Types
+/**
+    Mail-Model in der Datenbank.
+*/
+type Mail = Model & {
+    /**
+        ID der Mail.
+    */
+    id: string;
+
+    /**
+        ID des Senders.
+    */
+    senderId: string;
+
+    /**
+        ID des Empfängers.
+    */
+    recipientId: string;
+
+    /**
+        Inhalt der Mail.
+    */
+    content: string;
+
+    /**
+        Erstelldatum der Mail.
+    */
+    createdAt: string;
+};
+// #endregion
+
+/**
+    Mail-Model in der Datenbank.
+*/
+const MailModel = sequelize.define<Mail>(
+    "Mail",
+    {
+        id: {
+            type: DataTypes.STRING(20),
+            primaryKey: true,
+            allowNull: false,
+            unique: true
+        },
+        senderId: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            field: "sender_id"
+        },
+        recipientId: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            field: "recipient_id"
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            field: "created_at"
+        }
+    }
+);
+
+export default MailModel;
