b37134f4d6a3b1b60c99a86cf3ec569df46ef735
wgumenyuk <wlad@gumenyuk.de>
2022-08-31 22:07:22 +0200

    DashboardSubtitle hinzugefügt

---
+++ packages/frontend/desktop/src/components/DashboardSubtitle/DashboardSubtitle.svelte
@@ -0,0 +1,12 @@
+<span class="dashboard-subtitle">
+    <slot/>
+</span>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .dashboard-subtitle {
+        color: $white-lighter;
+        user-select: none;
+    }
+</style>
