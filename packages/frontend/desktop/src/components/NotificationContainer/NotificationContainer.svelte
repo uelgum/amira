496752607b7221b7930b2174f74c8406c3499041
wgumenyuk <wlad@gumenyuk.de>
2022-09-13 21:04:23 +0200

    WIP: NotificationContainer

---
+++ packages/frontend/desktop/src/components/NotificationContainer/NotificationContainer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+</script>
+
+<div class="notification-container">
+    Notification Container
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .notification-container {
+        position: absolute;
+        background: $black-light;
+        width: 25vw;
+        max-width: 25em;
+        max-height: 25em;
+        top: 3.5em;
+        right: 0em;
+        margin-right: auto;
+        padding: 1em;
+        border: 1px solid $black-lightest;
+        border-radius: 0.25em;
+    }
+</style>
