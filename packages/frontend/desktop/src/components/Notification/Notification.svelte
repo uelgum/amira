c547eead4b87fc62f44ddc19a6e97b02f018bf18
wgumenyuk <wlad@gumenyuk.de>
2022-09-14 22:28:04 +0200

    WIP Notification hinzugefügt

---
+++ packages/frontend/desktop/src/components/Notification/Notification.svelte
@@ -0,0 +1,15 @@
+<div class="notification">
+    <span>
+        <slot/>
+    </span>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+    
+    .notification {
+        background: $black-lighter;
+        padding: 0.5em;
+        border-radius: 0.25em;
+    }
+</style>
