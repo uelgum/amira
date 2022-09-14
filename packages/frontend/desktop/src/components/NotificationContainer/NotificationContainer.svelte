0d8a825f6b982bcac547399213390e157f23b24b
wgumenyuk <wlad@gumenyuk.de>
2022-09-14 22:28:58 +0200

    WIP NotificationContainer

--- packages/frontend/desktop/src/components/NotificationContainer/NotificationContainer.svelte
+++ packages/frontend/desktop/src/components/NotificationContainer/NotificationContainer.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-
+    import Notification from "@components/Notification";
 </script>
 
 <div class="notification-container">
-    Notification Container
+    <Notification>
+        Das ist ein Test
+    </Notification>
+    <Notification>
+        Das ist auch ein Test
+    </Notification>
 </div>
 
 <style lang="scss">
@@ -11,7 +16,9 @@
 
     .notification-container {
         position: absolute;
+        display: flex;
         background: $black-light;
+        color: $white-light;
         width: 25vw;
         max-width: 25em;
         max-height: 25em;
@@ -21,5 +28,8 @@
         padding: 1em;
         border: 1px solid $black-lightest;
         border-radius: 0.25em;
+        flex-direction: column;
+        gap: 0.5em;
+        overflow-y: auto;
     }
 </style>
