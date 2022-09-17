8687613b434bb84066c8c111a8c3cabaa5a46db8
wgumenyuk <wlad@gumenyuk.de>
2022-09-17 18:15:08 +0200

    Hintergrund verändert, Transition hinzugefügt

--- packages/frontend/desktop/src/components/Notification/Notification.svelte
+++ packages/frontend/desktop/src/components/Notification/Notification.svelte
@@ -8,9 +8,15 @@
     @use "@amira/shared/scss/colors" as *;
     
     .notification {
-        background: $black-lighter;
+        background: $black-light;
         padding: 0.5em;
         border-radius: 0.25em;
         font-size: 0.85em;
+        transition: all 0.25s;
+        cursor: pointer;
+
+        &:hover {
+            background: $black-lighter;
+        }
     }
 </style>
