c666bbafc86a6532c187c258733495f5f8499f67
wgumenyuk <wlad@gumenyuk.de>
2022-09-18 22:36:38 +0200

    Seperator hinzugefügt

---
+++ packages/frontend/shared/components/Seperator/Seperator.svelte
@@ -0,0 +1,12 @@
+<span class="seperator"/>
+
+<style lang="scss">
+    @use "../../scss/colors" as *;
+
+    .seperator {
+        display: block;
+        width: 100%;
+        margin: 0.5em 0em 0.5em 0em;
+        border-bottom: 1px solid $black-lightest;
+    }
+</style>
