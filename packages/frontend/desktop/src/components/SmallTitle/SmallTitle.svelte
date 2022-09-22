da0b922c197e598c5305a41d30c43088b1775a68
wgumenyuk <wlad@gumenyuk.de>
2022-09-22 22:24:23 +0200

    SmallTitle hinzugefügt

---
+++ packages/frontend/desktop/src/components/SmallTitle/SmallTitle.svelte
@@ -0,0 +1,14 @@
+<span class="small-title">
+    <slot/>
+</span>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .small-title {
+        color: $white-lightest;
+        margin-bottom: 0.85em;
+        font-weight: 600;
+        text-transform: uppercase;
+    }
+</style>
