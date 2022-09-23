e4a047d462f5c7f72193d88bcefc0f3cc7e89228
wgumenyuk <wlad@gumenyuk.de>
2022-09-23 12:55:00 +0200

    SmallDescription hinzugefügt

---
+++ packages/frontend/desktop/src/components/SmallDescription/SmallDescription.svelte
@@ -0,0 +1,13 @@
+<span class="small-description">
+    <slot/>
+</span>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .small-description {
+        color: $white-light;
+        font-size: 0.85em;
+        user-select: none;
+    }
+</style>
