903c5c6ea6a95f0f1e9e351d8b5759b1e77dfeb7
wgumenyuk <wlad@gumenyuk.de>
2022-09-24 20:15:45 +0200

    Center-Layout hinzugefügt

---
+++ packages/frontend/desktop/src/layouts/Center/Center.svelte
@@ -0,0 +1,14 @@
+<div class="center">
+    <slot/>
+</div>
+
+<style lang="scss">
+    .center {
+        display: flex;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 1em;
+    }
+</style>
