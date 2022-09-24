6a3d816b118efa6852b8f6189c9e3517d0cad0c5
wgumenyuk <wlad@gumenyuk.de>
2022-09-24 20:14:40 +0200

    TitleBarButtonIcon hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/TitleBarButtonIcon/TitleBarButtonIcon.svelte
@@ -0,0 +1,35 @@
+<script context="module" lang="ts">
+    // #region Types
+    export type TopBarIconButtonType = "minimize" | "maximize" | "close";
+    // #endregion
+</script>
+
+<script lang="ts">
+    import minimizeIcon from "@amira/shared/png/minimize.png";
+    import maximizeIcon from "@amira/shared/png/maximize.png";
+    import closeIcon from "@amira/shared/png/close.png";
+
+    /**
+        Typ des Icons. Kann entweder `minimize`, `maxmimize` oder `close` sein.
+    */
+    export let type: TopBarIconButtonType;
+
+    const icons = {
+        minimize: minimizeIcon,
+        maximize: maximizeIcon,
+        close: closeIcon
+    };
+</script>
+
+<img
+    src={icons[type]}
+    class="title-bar-button-icon"
+    alt="title-bar-button-icon"
+    draggable="false"
+/>
+
+<style lang="scss">
+    .title-bar-button-icon {
+        width: 0.75em;
+    }
+</style>
