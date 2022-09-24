98a91ab5ede143ad6e32f1dca3e6d17e3c6abbca
wgumenyuk <wlad@gumenyuk.de>
2022-09-24 20:14:12 +0200

    Loader hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Loader/Loader.svelte
@@ -0,0 +1,19 @@
+<div class="loader"/>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .loader {
+        width: 2.5em;
+        height: 2.5em;
+        border: 0.25em solid $black-lightest;
+        border-top: 0.25em solid $blue;
+        border-radius: 50%;
+        animation: loader-spin 2s linear infinite;
+    }
+
+    @keyframes loader-spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
