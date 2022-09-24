a500db41d222dd89a130e2b9e321ec3e3b68b2cf
wgumenyuk <wlad@gumenyuk.de>
2022-09-24 20:15:30 +0200

    Base-Layout hinzugefügt

---
+++ packages/frontend/desktop/src/layouts/Base/Base.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import TitleBar from "@organisms/TitleBar";
+</script>
+
+<TitleBar/>
+<div id="main">
+    <slot/>
+</div>
+
+<style lang="scss">
+    #main {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        overflow: hidden auto;
+    }
+</style>
