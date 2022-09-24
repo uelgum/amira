ac9485c46db657b9b56564a37248dbe680007b42
wgumenyuk <wlad@gumenyuk.de>
2022-09-24 20:15:14 +0200

    TitleBar hinzugefügt

---
+++ packages/frontend/desktop/src/components/organisms/TitleBar/TitleBar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import TitleBarControls from "@molecules/TitleBarControls";
+    import TitleBarTitle from "@atoms/TitleBarTitle";
+</script>
+
+<div id="title-bar" data-tauri-drag-region>
+    <TitleBarTitle/>
+    <TitleBarControls/>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+    
+    #title-bar {
+        display: flex;
+        background: $black;
+        border-radius: 0.5em 0.5em 0em 0em;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
