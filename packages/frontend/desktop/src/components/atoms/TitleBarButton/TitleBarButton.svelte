b30676d63c485de13c8ac4e397ca73871c152b66
wgumenyuk <wlad@gumenyuk.de>
2022-09-24 20:14:24 +0200

    TitleBarButton hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/TitleBarButton/TitleBarButton.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    /**
+        Knöpfe mit dem Attribut `close` werden beim Hovern rot eingefärbt.
+    */
+    export let close = false;
+</script>
+
+<button class="title-bar-button" class:close={close} on:click>
+    <slot/>
+</button>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .title-bar-button {
+        display: flex;
+        background: $black;
+        color: $white;
+        width: 2.5em;
+        padding: 0.5em;
+        border: none;
+        outline: none;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+
+        &:hover {
+            background: $black-light;
+        }
+    }
+
+    .title-bar-button.close {
+        border-top-right-radius: 0.5em;
+
+        &:hover {
+            background: $red;
+        }
+    }
+</style>
---
+++ packages/frontend/desktop/src/components/atoms/TitleBarButton/index.ts
@@ -0,0 +1,1 @@
+export { default } from "./TitleBarButton.svelte";
