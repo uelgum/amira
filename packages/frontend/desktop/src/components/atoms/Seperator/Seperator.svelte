6b1fa71b6dc8f03ea5dedd143c9ce9d84b78a519
wgumenyuk <wlad@gumenyuk.de>
2022-09-25 19:28:57 +0200

    Seperator hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Seperator/Seperator.svelte
@@ -0,0 +1,10 @@
+<div class="seperator"/>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .seperator {
+        width: 100%;
+        border-bottom: 1px solid $black-lightest;
+    }
+</style>
