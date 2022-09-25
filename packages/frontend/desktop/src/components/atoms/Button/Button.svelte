f4ef7718413a2fd701148d107616e789e9014a16
wgumenyuk <wlad@gumenyuk.de>
2022-09-25 19:30:09 +0200

    Button hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Button/Button.svelte
@@ -0,0 +1,25 @@
+<button class="button" on:click>
+    <slot/>
+</button>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .button {
+        background: $blue;
+        color: $white-light;
+        padding: 0.75em;
+        border: 1px solid $blue;
+        border-radius: 0.5em;
+        outline: none;
+        font-family: "Inter", sans-serif;
+        font-size: 1em;
+        letter-spacing: 0.1em;
+        cursor: pointer;
+        transition: background 0.1s ease-in;
+
+        &:hover {
+            background: darken($blue, 3);
+        }
+    }
+</style>
