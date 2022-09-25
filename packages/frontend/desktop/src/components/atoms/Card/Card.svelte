a392b9133d502f72196c1d2d8cb710a173063021
wgumenyuk <wlad@gumenyuk.de>
2022-09-25 19:28:21 +0200

    Card hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Card/Card.svelte
@@ -0,0 +1,15 @@
+<div class="card">
+    <slot/>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .card {
+        position: relative;
+        background: $black-lighter;
+        padding: 1.5em;
+        border: 1px solid $black-lightest;
+        border-radius: 0.5em;
+    }
+</style>
