47a019e27363aad39f66f6aafc11f94a1df85394
wgumenyuk <wlad@gumenyuk.de>
2022-09-25 19:28:36 +0200

    Label hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Label/Label.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    /**
+        ID des Elements, zu dem das Label gehört.
+    */
+    let forId: string;
+    export { forId as for };
+</script>
+
+<label class="label" for={forId}>
+    <slot/>    
+</label>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .label {
+        display: flex;
+        color: $white-lighter;
+        font-size: 0.85em;
+        font-weight: 600;
+        flex-direction: column;
+        gap: 0.5em;
+        user-select: none;
+    }
+</style>
