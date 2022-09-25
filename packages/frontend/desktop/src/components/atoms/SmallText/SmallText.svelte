0f3cb8b47894a7659a55c0af82cf3c6a5e4b8e17
wgumenyuk <wlad@gumenyuk.de>
2022-09-25 19:29:16 +0200

    SmallText hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/SmallText/SmallText.svelte
@@ -0,0 +1,33 @@
+<script context="module" lang="ts">
+    // #region Types
+    export type Color =
+        "white" | "white-light" | "white-lighter" |
+        "white-lightest" | "red" | "green";
+    // #endregion
+</script>
+
+<script lang="ts">
+    /**
+        Farbe des Textes.
+    */
+    export let color: Color = "white";
+</script>
+
+<span class="small-text {color}">
+    <slot/>
+</span>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+    
+    .small-text {
+        font-size: 0.85em;
+
+        &.white { color: $white; }
+        &.white-light { color: $white-light; }
+        &.white-lighter { color: $white-lighter; }
+        &.white-lightest { color: $white-lightest; }
+        &.red { color: $red; }
+        &.green { color: $green; }
+    }
+</style>
