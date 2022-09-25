2d5f52ab73f739fc8a536772e4882e7f81fa13b5
wgumenyuk <wlad@gumenyuk.de>
2022-09-25 19:28:47 +0200

    Link hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Link/Link.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    /**
+        Ziel des Links.
+    */
+    export let to: string;
+</script>
+
+<a class="link" href={to}>
+    <slot/>
+</a>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .link {
+        color: $blue;
+        border-bottom: 1px solid transparent;
+        text-decoration: none;
+
+        &:hover {
+            border-bottom: 1px solid $blue;
+        }
+
+        &::selection {
+            color: invert($blue);   
+        }
+    }
+</style>
