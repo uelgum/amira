e5368ff48d2db7d57d8494df79ea0be12c14fd67
wgumenyuk <wlad@gumenyuk.de>
2022-09-27 16:15:15 +0200

    CardTitle hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/CardTitle/CardTitle.svelte
@@ -0,0 +1,17 @@
+<span class="card-title">
+    <slot/>
+</span>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .card-title {
+        display: block;
+        color: $white-light;
+        margin-bottom: 2em;
+        font-size: 0.85em;
+        font-weight: 600;
+        text-align: center;
+        user-select: none;
+    }
+</style>
