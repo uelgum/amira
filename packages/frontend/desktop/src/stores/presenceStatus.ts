66e9906c39ab43402b701bec9f6d843837ce703e
wgumenyuk <wlad@gumenyuk.de>
2022-09-27 18:01:22 +0200

    Store für Presence-Status hinzugefügt

---
+++ packages/frontend/desktop/src/stores/presenceStatus.ts
@@ -0,0 +1,32 @@
+import { writable } from "svelte/store";
+
+// Intern
+import socket from "@internal/socket";
+import { PRESENCE_STATUS_KEY } from "@internal/constants";
+
+// #region Types
+type PresenceStatus = "online" | "away" | "dnd" | "offline";
+// #endregion
+
+/**
+    Presence-Status des Nutzers.
+*/
+const presenceStatus = writable<PresenceStatus>(
+    window.localStorage.getItem(PRESENCE_STATUS_KEY) as PresenceStatus || "online"
+);
+
+/**
+    Aktualisiert den Wert im Store.
+*/
+const set = (status: PresenceStatus) => {
+    window.localStorage.setItem(PRESENCE_STATUS_KEY, status);
+
+    if(socket.connected) {
+        socket.emit("presenceUpdate", { status });
+    }
+};
+
+export default {
+    subscribe: presenceStatus.subscribe,
+    set
+};
