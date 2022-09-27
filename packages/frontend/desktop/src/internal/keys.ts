79f34e33f4c7d7e2bf9f3342fb2da5c80bf0bd4f
wgumenyuk <wlad@gumenyuk.de>
2022-09-27 18:02:35 +0200

    Key-Funktionen hinzugefügt

---
+++ packages/frontend/desktop/src/internal/keys.ts
@@ -0,0 +1,60 @@
+import { readTextFile, writeTextFile } from "@tauri-apps/api/fs";
+import { join } from "@tauri-apps/api/path";
+import * as openpgp from "openpgp";
+
+// Intern
+import { getAppDir } from "@internal/app";
+
+/**
+    Generiert ein OpenPGP-Schlüsselpaar.
+*/
+const generatePrivateKey = async (fullName: string, email: string) => {
+    const { privateKey, publicKey } = await openpgp.generateKey({
+        userIDs: [
+            {
+                comment: "Amira",
+                name: fullName,
+                email
+            }
+        ],
+        format: "armored"
+    });
+
+    return { privateKey, publicKey };
+};
+
+/**
+    Speichert den Private Key in den App-Dateien.
+*/
+const writePrivateKey = async (privateKey: string) => {
+    const appDir = await getAppDir();
+    const filePath = await join(appDir, "private.pem");
+
+    await writeTextFile(filePath, privateKey);
+};
+
+/**
+    Lädt den Private Key aus den App-Dateien.
+*/
+const readPrivateKey = async () => {
+    const appDir = await getAppDir();
+    const filePath = await join(appDir, "private.pem");
+
+    const content = await readTextFile(filePath);
+
+    const armoredKey = await openpgp.readPrivateKey({
+        armoredKey: content
+    });
+
+    const privateKey = await openpgp.decryptKey({
+        privateKey: armoredKey
+    });
+
+    return privateKey;
+};
+
+export {
+    generatePrivateKey,
+    writePrivateKey,
+    readPrivateKey
+};
