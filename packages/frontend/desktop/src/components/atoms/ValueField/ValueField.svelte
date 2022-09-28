fa67cacc843dd40040dd4d8b2f2ee11039dd34e3
wgumenyuk <wlad@gumenyuk.de>
2022-09-28 16:45:11 +0200

    ValueField hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/ValueField/ValueField.svelte
@@ -0,0 +1,18 @@
+<div class="value-field">
+    <slot/>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .value-field {
+        display: flex;
+        background: $black-even-lighter;
+        color: $white-light;
+        padding: 0.75em;
+        border: 1px solid $black-lightest;
+        border-radius: 0.25em;
+        justify-content: space-around;
+        align-items: center;
+    }
+</style>
