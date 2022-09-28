c3f75aef5501443580872c93a6dccb0cfcc4b472
wgumenyuk <wlad@gumenyuk.de>
2022-09-28 23:13:33 +0200

    navigate hinzugefügt

--- packages/frontend/desktop/src/events/connectError.ts
+++ packages/frontend/desktop/src/events/connectError.ts
@@ -1,9 +1,11 @@
 import socket from "@internal/socket";
 import socketConnection from "@stores/socketConnection";
+import { navigate } from "svelte-routing";
 
 /**
     Wird ausgeführt, wenn die Socket-Verbindung scheitert.
 */
 socket.on("connect_error", () => {
     socketConnection.set(false);
+    navigate("/connecting");
 });
