af099c7897446f79cc441e35a366106ac7dbc0e2
wgumenyuk <wlad@gumenyuk.de>
2022-09-29 22:16:19 +0200

    Konstante für Side-Bar hinzugefügt

--- packages/frontend/desktop/src/internal/constants.ts
+++ packages/frontend/desktop/src/internal/constants.ts
@@ -9,6 +9,11 @@ export const TOKEN_KEY = "TOKEN";
 export const PRESENCE_STATUS_KEY = "PRESENCE_STATUS";
 
 /**
+    Schlüssel für den Zustand der Side-Bar.
+*/
+export const SIDE_BAR_MINIMIZED_KEY = "SIDE_BAR_MINIMIZED";
+
+/**
     Schüssel für den Lockscreen.
 */
 export const LOCKED_KEY = "LOCKED";
