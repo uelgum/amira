3b6b493aba364c684726bd009a577d29f65f3f3c
wgumenyuk <wlad@gumenyuk.de>
2022-10-05 16:20:14 +0200

    Bree-Loader verbessert

--- packages/backend/src/loaders/bree.ts
+++ packages/backend/src/loaders/bree.ts
@@ -1,6 +1,9 @@
 import path from "path";
 import Bree from "bree";
 
+// Intern
+import logger from "@loaders/logger";
+
 /**
     Pfad zu den Jobs.
 */
@@ -12,7 +15,32 @@ const JOBS_PATH = path.join(__dirname, "../jobs");
 const bree = new Bree({
     logger: false,
     root: JOBS_PATH,
-    jobs: []
+    jobs: [
+        {
+            name: "dailies",
+            interval: "at 12:00 am"
+        }
+    ]
 });
+
+/**
+    Initialisiert den Job-Scheduler.
+*/
+const loadJobs = async () => {
+    const start = performance.now();
+
+    await bree.start();
+
+    const jobs = bree.config.jobs.length;
+
+    const end = performance.now();
+    const duration = Math.round(end - start);
+
+    logger.info(`Jobs geladen (insgesamt ${jobs}) (${duration}ms)`);
+};
+
+export {
+    loadJobs
+};
 
 export default bree;
