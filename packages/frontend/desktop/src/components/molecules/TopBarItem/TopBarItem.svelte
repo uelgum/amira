ea76f4d31804807fa78aa7e6243348a7ce4cb6e1
wgumenyuk <wlad@gumenyuk.de>
2022-10-07 12:57:23 +0200

    Verbessert

--- packages/frontend/desktop/src/components/molecules/TopBarItem/TopBarItem.svelte
+++ packages/frontend/desktop/src/components/molecules/TopBarItem/TopBarItem.svelte
@@ -6,10 +6,9 @@
     @use "@amira/shared/scss/colors" as *;
 
     .top-bar-item {
-        position: relative;
         padding: 0.5em;
         border-radius: 0.25em;
-        transition: background 0.25s ease-in-out;
+        transition: background 0.25s ease;
         cursor: pointer;
 
         &:hover {
