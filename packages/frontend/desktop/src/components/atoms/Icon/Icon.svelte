11dcbf9099ae789787cd0dde7c21ad2c95e6b76a
wgumenyuk <wlad@gumenyuk.de>
2022-10-07 09:21:15 +0200

    Icon hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Icon/Icon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    /**
+        Pfad zum Icon.
+    */
+    export let src: string;
+</script>
+
+<img
+    class="icon"
+    src={src}
+    alt="icon"
+    draggable="false"
+/>
+
+<style lang="scss">
+    .icon {
+        display: block;
+        user-select: none;
+    }
+</style>
