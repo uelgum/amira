880dbde295695fd27ae34c39ccbe8020f6fafff5
wgumenyuk <wlad@gumenyuk.de>
2022-10-07 09:21:44 +0200

    TopBarItem hinzugefügt

---
+++ packages/frontend/desktop/src/components/molecules/TopBarItem/TopBarItem.svelte
@@ -0,0 +1,19 @@
+<div class="top-bar-item" on:click>
+    <slot/>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .top-bar-item {
+        position: relative;
+        padding: 0.5em;
+        border-radius: 0.25em;
+        transition: background 0.25s ease-in-out;
+        cursor: pointer;
+
+        &:hover {
+            background: $black-even-lighter;
+        }
+    }
+</style>
