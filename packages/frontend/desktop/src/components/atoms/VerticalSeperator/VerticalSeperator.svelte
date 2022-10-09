7a575ced8625395e7f716f5e03b8510a3910c9de
wgumenyuk <wlad@gumenyuk.de>
2022-10-09 15:59:43 +0200

    VerticalSeperator hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/VerticalSeperator/VerticalSeperator.svelte
@@ -0,0 +1,11 @@
+<div class="vertical-seperator"/>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .vertical-seperator {
+        display: block;
+        height: 1.5em;
+        border-right: 1px solid $black-lightest;
+    }
+</style>
