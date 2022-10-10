74722c5adad6b4d11a09ced880bc1ce04df2ae74
wgumenyuk <wlad@gumenyuk.de>
2022-10-10 15:26:05 +0200

    position -> relative

--- packages/frontend/desktop/src/components/molecules/TopBarItem/TopBarItem.svelte
+++ packages/frontend/desktop/src/components/molecules/TopBarItem/TopBarItem.svelte
@@ -6,6 +6,7 @@
     @use "@amira/shared/scss/colors" as *;
 
     .top-bar__item {
+        position: relative;
         padding: 0.5em;
         border-radius: 0.25em;
         transition: background 0.25s ease;
