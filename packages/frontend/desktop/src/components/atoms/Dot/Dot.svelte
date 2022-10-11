770e3594e35e0b115861bc95d8efff13686edfeb
wgumenyuk <wlad@gumenyuk.de>
2022-10-11 19:59:50 +0200

    Dot hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Dot/Dot.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { fade } from "svelte/transition";
+
+    // Types
+    import type { FadeParams } from "svelte/transition";
+
+    const fadeParams: FadeParams = {
+        duration: 250
+    };
+</script>
+
+<div class="dot" in:fade={fadeParams} out:fade={fadeParams}/>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .dot {
+        position: absolute;
+        display: block;
+        background: $red;
+        width: 6px;
+        height: 6px;
+        right: 0.25em;
+        bottom: 0.35em;
+        border-radius: 50%;
+        pointer-events: none;
+    }
+</style>
