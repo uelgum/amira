57dc824ddfae2f96c3931372e6ac43ed5a04527c
wgumenyuk <wlad@gumenyuk.de>
2022-10-13 19:26:59 +0200

    PresenceStatus hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/PresenceStatus/PresenceStatus.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" context="module">
+    // #region Types
+    type Status = "online" | "away" | "dnd" | "offline";
+    // #endregion
+</script>
+
+<script lang="ts">
+    /**
+        Typ des Presence-Status.
+    */
+    export let type: Status;
+</script>
+
+<div class="presence-status {type}"/>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .presence-status {
+        display: block;
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 50%;
+        transition: background 0.25s ease;
+
+        &.online { background: $green; }
+        &.away { background: $yellow; }
+        &.dnd { background: $red; }
+        &.offline { background: lighten($black-lightest, 15); }
+    }
+</style>
