61caf84c8b4f5e34b4ad8aa0f8061fefc97b65bc
wgumenyuk <wlad@gumenyuk.de>
2022-10-13 19:27:45 +0200

    NotificationContainer hinzugefügt

---
+++ packages/frontend/desktop/src/components/molecules/NotificationContainer/NotificationContainer.svelte
@@ -0,0 +1,15 @@
+<div id="notification-container">
+    <slot/>
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    #notification-container {
+        display: flex;
+        max-height: 12em;
+        flex-direction: column;
+        gap: 0.5em;
+        overflow: hidden auto;
+    }
+</style>
