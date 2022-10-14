1e8156a8b5c1ab3193197255ac5668d26728c576
wgumenyuk <wlad@gumenyuk.de>
2022-10-14 20:48:39 +0200

    TopBar verbessert

--- packages/frontend/desktop/src/components/organisms/TopBar/TopBar.svelte
+++ packages/frontend/desktop/src/components/organisms/TopBar/TopBar.svelte
@@ -1,14 +1,52 @@
 <script lang="ts">
+    import Dot from "@atoms/Dot";
     import Icon from "@atoms/Icon";
+    import VerticalSeperator from "@atoms/VerticalSeperator";
     import TopBarItem from "@molecules/TopBarItem";
+    import CurrentUser from "@molecules/CurrentUser";
+    import NotificationMenu from "@organisms/NotificationMenu";
+    import UserMenu from "@organisms/UserMenu";
 
     // Intern
     import sideBarExpanded from "@stores/sideBarExpanded";
+    import notifications from "@stores/notifications";
 
     // Icons
     import menuIcon from "@amira/shared/svg/menu.svg";
     import bellIcon from "@amira/shared/svg/bell.svg";
     import downloadIcon from "@amira/shared/svg/downloadGreen.svg";
+
+    let menu = "";
+
+    /**
+        Überprüft, ob das Target ein Notification-Element ist.
+    */
+    const isNotificationItem = (target: HTMLElement) => {
+        if(target.className.startsWith("top-bar__item")) return false;
+        
+        // Das gesamte Benachrichtigungs-Menü ignorieren
+        const shouldIgnore =
+            target.id.startsWith("notification") ||
+            target.className.startsWith("notification");
+
+        return shouldIgnore;
+    };
+
+    /**
+        Überprüft, ob das Target ein Presence-Element ist.
+    */
+    const isPresenceItem = (target: HTMLElement) => {
+        const child = target.children[0];
+
+        if(!child) return false;
+
+        // Den aktuellen Presence-Selektor und den Zurück-Knopf ignorieren
+        const shouldIgnore =
+            (child.id === "user-menu__presence-selector") ||
+            child.className.startsWith("icon") && child.getAttribute("src") !== bellIcon;
+
+        return shouldIgnore;
+    };
 
     /**
         Schaltet die Side-Bar um.
@@ -16,24 +54,79 @@
     const toggleSideBar = () => {
         $sideBarExpanded = !$sideBarExpanded;
     };
+
+    /**
+        Schaltet ein Menü um.
+    */
+    const toggleMenu = (event: Event, newMenu: string) => {
+        const target = event.target as HTMLElement;
+
+        // Notification-Elemente ignorieren
+        if(isNotificationItem(target)) return;
+
+        // Presence-Selector ignorieren
+        if(isPresenceItem(target)) return;
+
+        // User-Menü ignorieren
+        if(target.id.startsWith("user-menu")) return;
+
+        menu = (menu === newMenu) ? "" : newMenu;
+    };
+
+    /**
+        Wird ausgeführt, sobald der Nutzer außerhalb des offenen Menüs klickt.
+    */
+    const handleOutsideClick = (event: Event) => {
+        const target = event.target as HTMLElement;
+
+        // Top-Bar-Elemente ignorieren
+        const topBarButtons = document.querySelector("#top-bar__buttons");
+        const isTopBarButton = topBarButtons.contains(target);
+
+        if(isTopBarButton) return;
+
+        // Notification-Elemente ignorieren
+        if(isNotificationItem(target)) return;
+
+        // Presence-Elemente ignorieren
+        if(isPresenceItem(target)) return;
+
+        // Offenes Menü verstecken
+        menu = "";
+    };
 </script>
 
+<svelte:body on:click={handleOutsideClick}/>
+
 <div id="top-bar">
+    <!-- Switch für Side-Bar -->
     <TopBarItem on:click={toggleSideBar}>
         <Icon src={menuIcon}/>
     </TopBarItem>
 
-    <div>
-        <!-- TODO Konditionell anzeigen -->
-        <TopBarItem>
-            <Icon src={downloadIcon}/>
-        </TopBarItem>
-
-        <TopBarItem>
+    <!-- Knöpfe und Menüs -->
+    <div id="top-bar__buttons">
+        <TopBarItem on:click={(event) => toggleMenu(event, "notification")}>
+            {#if $notifications.length > 0}
+                <Dot/>
+            {/if}
+            
             <Icon src={bellIcon}/>
+
+            {#if menu === "notification"}
+                <NotificationMenu/>
+            {/if}
         </TopBarItem>
 
-        <!-- TODO Current User hinzufügen -->
+        <VerticalSeperator/>
+
+        <TopBarItem on:click={(event) => toggleMenu(event, "user")}>
+            <CurrentUser/>
+
+            {#if menu === "user"}
+                <UserMenu/>
+            {/if}
+        </TopBarItem>
     </div>
 </div>
 
@@ -49,10 +142,11 @@
         border-bottom: 1px solid $black-lightest;
         justify-content: space-between;
         align-items: center;
+    }
 
-        div {
-            display: flex;
-            gap: 0.5em;
-        }
+    #top-bar__buttons {
+        display: flex;
+        align-items: center;
+        gap: 0.5em;
     }
 </style>
