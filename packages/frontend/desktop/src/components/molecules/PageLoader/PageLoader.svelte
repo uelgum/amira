853da530137e79aa0d80cb71156c4f29305b7823
wgumenyuk <wlad@gumenyuk.de>
2022-10-16 19:23:16 +0200

    PageLoader hinzugefügt

---
+++ packages/frontend/desktop/src/components/molecules/PageLoader/PageLoader.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Loader from "@atoms/Loader";
+
+    // Layouts
+    import Center from "@layouts/Center";
+
+    /**
+        Ob der Inhalt geladen ist.
+    */
+    export let isLoaded: boolean;
+</script>
+
+{#if !isLoaded}
+    <Center>
+        <Loader/>
+    </Center>
+{:else}
+    <slot/>
+{/if}
