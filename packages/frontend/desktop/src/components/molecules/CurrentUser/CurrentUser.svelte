6073a6ab96ac0c1a1d52dc8431306f5cedd4bda8
wgumenyuk <wlad@gumenyuk.de>
2022-10-16 19:16:01 +0200

    CurrentUser hinzugefügt

---
+++ packages/frontend/desktop/src/components/molecules/CurrentUser/CurrentUser.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import Avatar from "@atoms/Avatar";
+    import SmallText from "@atoms/SmallText";
+    import PresenceStatus from "@atoms/PresenceStatus";
+
+    // Intern
+    import token from "@stores/token";
+    import presenceStatus from "@stores/presenceStatus";
+</script>
+
+{#if $token.raw}
+    <div id="current-user">    
+        <SmallText color="white-light">
+            Hallo, {$token.data.firstName}
+        </SmallText>
+        <div id="current-user__presence-status-container">
+            <Avatar id={$token.data.id} size="xs"/>
+            <div id="current-user__presence-status">
+                <PresenceStatus type={$presenceStatus}/>
+            </div>
+        </div>
+    </div>
+{/if}
+
+<style lang="scss">
+    #current-user {
+        position: relative;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 0.5em;
+        user-select: none;
+    }
+
+    #current-user__presence-status-container {
+        position: relative;
+    }
+
+    #current-user__presence-status {
+        position: absolute;
+        right: -0.15em;
+        bottom: -0.15em;
+    }
+</style>
