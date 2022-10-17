25a2a71b933a77c25e059151f43d8a6995481550
wgumenyuk <wlad@gumenyuk.de>
2022-10-17 17:02:31 +0200

    Presence-Update hinzugefügt

---
+++ packages/backend/src/api/events/presenceUpdate.ts
@@ -0,0 +1,33 @@
+import { PresenceType, sendPresenceUpdate } from "@services/notification";
+
+// Types
+import type { Socket } from "socket.io";
+import type SocketManager from "@structs/socketManager";
+
+// #region Types
+type Data = {
+    status: PresenceType;
+};
+// #endregion
+
+const allowedStatusTypes = Object.values(PresenceType);
+
+/**
+    Handler für das `connect`-Event.
+    Wird ausgeführt, sobald ein Socket eine Verbindung herstellt.
+*/
+const onPresenceUpdate = (socketManager: SocketManager, socket: Socket, data: Data) => {
+    const socketId = socket.user.id;
+    const status = data.status;
+
+    if(!allowedStatusTypes.includes(status)) {
+        return;
+    }
+
+    console.log("Presence Update:", status);
+
+    socketManager.presence.set(socketId, status);
+    sendPresenceUpdate(socketId, status);
+};
+
+export default onPresenceUpdate;
