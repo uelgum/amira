809a3257cc3dae75aebdd4bbc97f179cdf262a9d
wgumenyuk <wlad@gumenyuk.de>
2022-10-21 21:24:27 +0200

    Notification-Routes überarbeitet

--- packages/backend/src/api/routes/notification.ts
+++ packages/backend/src/api/routes/notification.ts
@@ -1,23 +1,27 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
+// Intern
 import isLoggedIn from "@api/middleware/http/isLoggedIn";
 import { sendData, sendError } from "@utils/response";
 import { deleteNotification, getNotifications } from "@services/notification";
 
+// Types
+import type { Request, Response } from "express";
+
 /**
-    Notification-Router.
+    Notifications-Router.
 */
 const router = Router();
 
 router.use(isLoggedIn);
 
 /**
-    GET /api/notification/get
-    Ruft alle Benachrichtigungen ab.
+    GET /api/notifications
+    Ruft alle Benachrichtigungen eines Nutzers ab.
 */
-router.get("/get", async (req: Request, res: Response) => {
+router.get("/", async (req: Request, res: Response) => {
     try {
-        const data = await getNotifications(req.user.id);
+        const data = await getNotifications(req);
         sendData(res, data);
     } catch(error: any) {
         sendError(res, error);
@@ -25,8 +29,8 @@ router.get("/get", async (req: Request, res: Response) => {
 });
 
 /**
-    POST /api/notification/delete/:notificationId
-    Entfernt eine Benachrichtung.
+    POST /api/notifications/delete/:notificationId
+    Löscht eine Benachrichtigung eines Nutzers.
 */
 router.post("/delete/:notificationId", async (req: Request, res: Response) => {
     try {
