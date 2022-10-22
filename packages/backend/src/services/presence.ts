609cba1f358b129e6b7b5dfac60989ce4f40c175
wgumenyuk <wlad@gumenyuk.de>
2022-10-22 23:31:25 +0200

    Presence-Service hinzugefügt

---
+++ packages/backend/src/services/presence.ts
@@ -0,0 +1,53 @@
+import { Op } from "sequelize";
+
+// Intern
+import sockets from "@loaders/sockets";
+import Contact, { ContactStatus } from "@models/contact";
+
+// #region Types
+/**
+    Art des Presence-Status.
+*/
+enum PresenceStatusType {
+    ONLINE = "online",
+    AWAY = "away",
+    DND = "dnd",
+    OFFLINE = "offline"
+};
+// #endregion
+
+/**
+    Schickt ein Presence-Update an alle Kontakte eines Nutzers.
+*/
+const sendPresenceUpdate = async (userId: string, presenceStatus: PresenceStatusType) => {
+    const contacts = await Contact.findAll({
+        where: {
+            [ Op.or ]: [
+                { userId1: userId },
+                { userId2: userId }
+            ],
+            status: ContactStatus.CONFIRMED
+        }
+    });
+
+    if(contacts.length === 0) return;
+
+    for(const contact of contacts) {
+        if(!sockets.has(contact.id)) continue;
+
+        const socket = sockets.get(contact.id);
+
+        socket.emit("presenceUpdate", {
+            userId,
+            status: presenceStatus
+        });
+    }
+};
+
+export {
+    PresenceStatusType
+};
+
+export {
+    sendPresenceUpdate
+};
