08274c6d4f74f66f550f4e5dd70eca971d1d177b
wgumenyuk <wlad@gumenyuk.de>
2022-10-24 16:21:43 +0200

    Notification verbessert

--- packages/frontend/desktop/src/components/atoms/Notification/Notification.svelte
+++ packages/frontend/desktop/src/components/atoms/Notification/Notification.svelte
@@ -1,21 +1,41 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
+    // Komponente
     import SmallText from "@atoms/SmallText";
+
+    // Assets
+    import notifications from "@amira/shared/json/notifications.json";
+
+    // Types
+    import type { Notification } from "@stores/notifications";
 
     /**
         Erstelldatum der Benachrichtigung.
     */
-    export let createdAt: number;
+    export let notification: Notification;
 
-    const date = new Date(createdAt);
+    let content: string = notifications[notification.type];
+
+    const date = new Date(notification.createdAt);
 
     const timestamp = date.toLocaleTimeString("de-DE", {
         hour: "2-digit",
         minute: "2-digit"
     });
+
+    /**
+        On-Mount.
+    */
+    onMount(() => {
+        for(const [ variable, value ] of Object.entries(notification.data)) {
+            content = content.replace(`{${variable}}`, `${value}`);
+        }
+    });
 </script>
 
 <div class="notification" on:click>
-    <slot/>
+    {content}
     <SmallText color="white-lightest">
         {timestamp}
     </SmallText>
@@ -27,6 +47,7 @@
     .notification {
         display: flex;
         color: $white-light;
+        width: 100%;
         margin-right: 0.5em;
         padding: 0.5em;
         border-radius: 0.25em;
