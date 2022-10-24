ed6978c20f4fc43fde378d44585571d6aa9b2cfe
wgumenyuk <wlad@gumenyuk.de>
2022-10-24 17:31:42 +0200

    Notification-Event verbessert

--- packages/frontend/desktop/src/events/notification.ts
+++ packages/frontend/desktop/src/events/notification.ts
@@ -1,15 +1,28 @@
+import api from "@internal/api";
 import socket from "@internal/socket";
 import notifications from "@stores/notifications";
 
 // Types
 import type { Notification } from "@stores/notifications";
 
+// #region Types
+type Response = {
+    notifications: Notification[];
+};
+// #endregion
+
 /**
     Wird ausgeführt, wenn die Socket-Verbindung scheitert.
 */
-socket.on("notification", (data: Notification) => {
+socket.on("notification", async () => {
+    const res = await api.get<Response>("/notifications");
+
+    if(res.status === "err") {
+        return;
+    }
+
     notifications.update((n) => [
-        data,
+        ...res.data.notifications,
         ...n
     ]);
 }); 
