2b9dc8c2b460c3ad2ab5dd6196d76d3809e545b1
wgumenyuk <wlad@gumenyuk.de>
2022-10-24 15:26:22 +0200

    Mail entfernt

--- packages/backend/src/api/routes/apps/index.ts
+++ packages/backend/src/api/routes/apps/index.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 
 // Intern
 import isLoggedIn from "@api/middleware/http/isLoggedIn";
-import mailRouter from "@api/routes/apps/mail";
 import notesRouter from "@api/routes/apps/notes";
 import taskRouter from "@api/routes/apps/task";
 import dailiesRouter from "@api/routes/apps/dailies";
@@ -14,7 +13,6 @@ const router = Router();
 
 router.use(isLoggedIn);
 
-router.use("/mail", mailRouter);
 router.use("/notes", notesRouter);
 router.use("/task", taskRouter);
 router.use("/dailies", dailiesRouter);
