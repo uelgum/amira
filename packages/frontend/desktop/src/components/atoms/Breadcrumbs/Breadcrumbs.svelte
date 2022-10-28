9c5d49a7c35191b4109e2ff808edf70f5f32a729
wgumenyuk <wlad@gumenyuk.de>
2022-10-28 21:12:28 +0200

    Breadcrumbs hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Breadcrumbs/Breadcrumbs.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    /**
+        Pfade für die Breadcrumbs.
+    */
+    export let paths: string[];
+
+    const lastPath = (paths.length > 1) ? paths.pop() : null;
+</script>
+
+<div class="breadcrumbs">
+    {#each paths as path}
+        <span class="breadcrumbs__path">{path}</span>
+    {/each}
+    {#if lastPath}
+        <span class="breadcrumbs__last-path">{lastPath}</span>
+    {/if}
+</div>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .breadcrumbs {
+        display: flex;
+        color: $white-lightest;
+        gap: 0.5em;
+        user-select: none;
+    }
+
+    .breadcrumbs__path {
+        position: relative;
+
+        &:not(:last-child) {
+            margin-right: 0.5em;
+        }
+
+        &:not(:last-child)::after {
+            content: "/";
+            position: absolute;
+            left: calc(100% + 0.25em);
+        }
+    }
+
+    .breadcrumbs__last-path {
+        color: lighten($blue, 5);
+        font-weight: 600;
+    }
+</style>
