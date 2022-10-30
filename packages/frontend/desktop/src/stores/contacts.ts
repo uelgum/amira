c9fd1f0699057047b50e45ccdd6b308139b2e629
wgumenyuk <wlad@gumenyuk.de>
2022-10-30 17:50:10 +0100

    Contact-Store erweitert

--- packages/frontend/desktop/src/stores/contacts.ts
+++ packages/frontend/desktop/src/stores/contacts.ts
@@ -1,11 +1,70 @@
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
+
+// Intern
+import api from "@internal/api";
 
 // Types
 import type { PresenceStatus } from "@stores/presenceStatus";
 
+// #region Types
+export type Contact = {
+    id: string;
+    contactId: string;
+    fullName: string;
+    presenceStatus: PresenceStatus;
+};
+
+type Response = {
+    contacts: Contact[];
+};
+// #endregion
+
+let isInit = false;
+
 /**
     Store für alle Kontakte und deren Presence-Status.
 */
-const contacts = writable<Record<string, PresenceStatus>>({});
+const contacts = writable<Contact[]>([]);
 
-export default contacts;
+/**
+    Store für alle Kontakte, die **online** sind.
+*/
+const onlineContacts = derived(
+    contacts,
+    ($contacts) => $contacts
+        .filter((c) => c.presenceStatus !== "offline")
+        .sort((a, b) => a.fullName.localeCompare(b.fullName))
+);
+
+/**
+    Store für alle Kontakte, die **offline** sind.
+*/
+const offlineContacts = derived(
+    contacts,
+    ($contacts) => $contacts
+        .filter((c) => c.presenceStatus === "offline")
+        .sort((a, b) => a.fullName.localeCompare(b.fullName))
+);
+
+/**
+    Initialisiert die Kontakt-Liste beim Start der App.
+*/
+const initContacts = async () => {
+    if(isInit) return;
+
+    const res = await api.get<Response>("/contacts");
+
+    if(res.status === "err") {
+        // TODO Etwas machen?
+        return;
+    }
+
+    contacts.set(res.data.contacts);
+};
+
+export {
+    initContacts,
+    contacts,
+    onlineContacts,
+    offlineContacts
+};
