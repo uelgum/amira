bf36fecfcf07e12c1942b71980e7cd2541201ab1
wgumenyuk <wlad@gumenyuk.de>
2022-10-31 20:25:34 +0100

    Title hinzugefügt

---
+++ packages/frontend/desktop/src/components/atoms/Title/Title.svelte
@@ -0,0 +1,13 @@
+<span class="title">
+    <slot/>
+</span>
+
+<style lang="scss">
+    @use "@amira/shared/scss/colors" as *;
+
+    .title {
+        color: $white;
+        font-size: 1.25em;
+        user-select: none;
+    }
+</style>
