c35bc4bc0e69d58db42b0a7d8503302bf2d0887d
wgumenyuk <wlad@gumenyuk.de>
2022-11-19 15:23:42 +0100

    404-Handler für App-Router hinzugefügt

--- packages/backend/src/api/routes/apps/index.ts
+++ packages/backend/src/api/routes/apps/index.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
 // Intern
+import AmiraError from "@structs/error";
+import { sendError } from "@utils/response";
+
+// Routen
 import isLoggedIn from "@api/middleware/http/isLoggedIn";
 import notesRouter from "@api/routes/apps/notes";
 import taskRouter from "@api/routes/apps/task";
 import dailiesRouter from "@api/routes/apps/dailies";
+
+// Types
+import type { Response } from "express";
 
 /**
     App-Router.
@@ -16,5 +23,10 @@ router.use(isLoggedIn);
 router.use("/notes", notesRouter);
 router.use("/task", taskRouter);
 router.use("/dailies", dailiesRouter);
+
+router.use((_, res: Response) => {
+    sendError(res, new AmiraError(404, "NOT_FOUND"));
+});
+
 
 export default router;
